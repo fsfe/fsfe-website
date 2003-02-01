cb2dfe8c4a24c9689bfae7abd3fc6dd0e349cba8
jonas <jonas@fsfe.org>
2003-02-01 14:39:34 +0000

    Fixed script.
    svn path=/trunk/; revision=3129

--- tools/build.sh
+++ tools/build.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 #
 # This is a simple wrapper that will build the web pages in two steps.
-# First, it will call build.pl with options that will not produce any
-# output files. If this run is successful, it will make a second run
-# and create the files.
+# First, it will call build.pl with options to send the output to a
+# temporary directory. After that is done, it will move the contents of
+# that directory over the normal root.
 #
 SOURCE=/home/www/fsfe
 DEST=/home/www/html
+TMP=/home/www/tmp
 
 cd $SOURCE
-tools/build.pl -n -o $DEST -i .
+tools/build.pl -q -o $TMP -i .
 
 if test $? -ne 0; then
    echo "Build not complete. Aborting."
    exit 1
 fi
-
-tools/build.pl -o $DEST -i .
-
-cd $DEST
+cd $TMP
 symlinks
+
+mv $DEST ${DEST}.old
+mv $TMP $DEST
+rm -rf ${DEST}.old
+

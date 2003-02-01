21865c0b26cfbdbd4d5172840cdc25cfd0304533
jonas <jonas@fsfe.org>
2003-02-01 12:16:52 +0000

    *** empty log message ***
    svn path=/trunk/; revision=3116

--- tools/build.sh
+++ tools/build.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
-TMP=/home/fsfe/tmp
-DEST=/home/www/
+#
+# This is a simple wrapper that will build the web pages in two steps.
+# First, it will call build.pl with options that will not produce any
+# output files. If this run is successful, it will make a second run
+# and create the files.
+#
+SOURCE=/home/www/fsfe/
+DEST=/home/www/html/
 
-tools/build.pl -o $TMP -i .
+tools/build.pl -n -o $DEST -i $SOURCE
+
 if test $? -ne 0; then
    echo "Build not complete. Aborting."
    exit 1
 fi
-rm -r $DEST/*
-mv $TMP/* $DEST/
 
+tools/build.pl -o $DEST -i $SOURCE

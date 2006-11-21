07208026e78012dede72307c72df70d73e6cf96b
reinhard <reinhard@fsfe.org>
2006-11-21 19:29:10 +0000

    More testing.
    svn path=/trunk/; revision=7304

--- cgi-bin/order.pl
+++ cgi-bin/order.pl
@@ -9,16 +9,15 @@ my $date = strftime "%Y-%m-%d", localtime;
 my $time = strftime "%s", localtime;
 my $reference = "order." . $date . "." . substr $time, -5;
 
-open(MAIL,"|/usr/lib/sendmail -t");
+open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
 print MAIL "From: mueller\@fsfeurope.org\n";
 print MAIL "To: mueller\@fsfeurope.org\n";
 print MAIL "Subject: Web order\n\n";
 print MAIL $reference . "\n\n";
 foreach $name ($query->param) {
-  foreach $value ($query->param($name)) {
-    if ($value) {
-      print MAIL $name . ": " . $value . "\n";
-    }
+  $value = $query->param($name)
+  if (not $name =~ /^_/ and $value) {
+    print MAIL $name . ": " . $value . "\n";
   }
 }
 close(MAIL);

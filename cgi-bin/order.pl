c12cc2ca6ac3bb33dec361dc61422183348be4f7
reinhard <reinhard@fsfe.org>
2006-11-21 11:19:52 +0000

    More experiments.
    svn path=/trunk/; revision=7298

--- cgi-bin/order.pl
+++ cgi-bin/order.pl
@@ -1,20 +1,27 @@
 #!/usr/bin/perl
 
 use CGI;
+use POSIX qw(strftime);
 
 $query = new CGI;
+@param = $query->param;
+
+my $date = strftime "%Y-%m-%d", localtime;
+my $time = strftime "%s", localtime;
+my $reference = "order." . $date . "." . substr $time, -5;
 
 open(MAIL,"|/usr/lib/sendmail -t");
 print MAIL "From: mueller\@fsfeurope.org\n";
 print MAIL "To: mueller\@fsfeurope.org\n";
 print MAIL "Subject: Web order\n\n";
-foreach $name ($query->param) {
-  foreach $value ($query->param($name)) {
+print MAIL $reference . "\n\n";
+foreach $name (@param) {
+  foreach $value (@param($name)) {
     if ($value) {
-      print MAIL $name.": ".$value."\n";
+      print MAIL $name . ": " . $value . "\n";
     }
   }
 }
 close(MAIL);
 
-print "Location: /order/thankyou.en.html\n\n";
+print "Location: /order/thankyou." . @param("language") . ".html\n\n";

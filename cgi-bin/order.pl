f35a11d9f616eb178b63184054059bdf93f5c658
reinhard <reinhard@fsfe.org>
2006-11-21 10:22:15 +0000

    Experimenting with order form.
    svn path=/trunk/; revision=7295

--- cgi-bin/order.pl
+++ cgi-bin/order.pl
@@ -3,17 +3,14 @@
 use CGI;
 
 $query = new CGI;
-@names = $query->param;
 
 open(MAIL,"|/usr/lib/sendmail -t");
 print MAIL "From: mueller\@fsfeurope.org\n";
 print MAIL "To: mueller\@fsfeurope.org\n";
 print MAIL "Subject: Web order\n\n";
-foreach(@names) {
-  $name = $_;
-  @values = $query->param($name);
-  foreach $value (@values) {
-    print MAIL $name.": ".$value."\n";
+foreach $name ($query->param) {
+  foreach $value ($query->param($name)) {
+    if ($value) print MAIL $name.": ".$value."\n";
   }
 }
 close(MAIL);

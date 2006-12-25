d14c8ccb4b512a476cc931d30370936137c6db2f
reinhard <reinhard@fsfe.org>
2006-12-25 18:03:08 +0000

    Calculate total amount.
    svn path=/trunk/; revision=7493

--- cgi-bin/order.pl
+++ cgi-bin/order.pl
@@ -7,19 +7,22 @@ my $query = new CGI;
 
 my $date = strftime "%Y-%m-%d", localtime;
 my $time = strftime "%s", localtime;
-my $reference = "order." . $date . "." . substr $time, -5;
+my $reference = "order.$date." . substr $time, -5;
+my $amount = 0;
 
 open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
 print MAIL "From: mueller\@fsfeurope.org\n";
 print MAIL "To: mueller\@fsfeurope.org\n";
 print MAIL "Subject: Web order\n\n";
-print MAIL $reference . "\n\n";
+print MAIL "$reference\n\n";
 foreach $name ($query->param) {
   $value = $query->param($name);
   if (not $name =~ /^_/ and $value) {
-    print MAIL $name . ": " . $value . "\n";
+    print MAIL "$name: $value\n";
+    $amount += $value * $query->param("_$name");
   }
 }
-close(MAIL);
+print MAIL "Total amount: $amount\n";
+close MAIL;
 
 print "Location: /order/thankyou." . $query->param("language") . ".html\n\n";

34546ef909b6704f2c3c2b91b131342838434f9c
reinhard <reinhard@fsfe.org>
2007-02-15 19:48:52 +0000

    Started to make build script more self contained.
    svn path=/trunk/; revision=7735

--- tools/build.sh
+++ tools/build.sh
@@ -1,45 +1,73 @@
-#! /bin/bash
-#
-# This is a simple wrapper that will build the web pages in two steps.
-# First, it will call build.pl with options to send the output to a
-# temporary directory. After that is done, it will move the contents of
-# that directory over the normal root.
-#
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Web page build script
+# -----------------------------------------------------------------------------
+# This script is called every 5 minutes on www.fsfeurope.org to rebuild the
+# HTML pages from the .xhtml, .xml and .xsl source files. Most of the work,
+# however, is done by the Perl script build.pl.
+# -----------------------------------------------------------------------------
+
 SOURCE=/home/www/fsfe
 DEST=/home/www/html
 TMP=/home/www/tmp.$$
 STATUS=/var/www/web
 
-cd $SOURCE
-tools/build.pl -q -o $TMP -i .
+# -----------------------------------------------------------------------------
+echo "$(date)  Building HTML pages"
+# -----------------------------------------------------------------------------
+
+cd ${SOURCE}
+tools/build.pl -q -o ${TMP} -i .
 
 if test $? -ne 0; then
-   echo "Build not complete. Aborting."
+   echo "$(date)  Build not complete. Aborting."
    exit 1
 fi
+
+# -----------------------------------------------------------------------------
+echo "$(date)  Linking source files"
+# -----------------------------------------------------------------------------
 
 for target in ${TMP}/*; do
   test -d ${target} && ln -s ${SOURCE} ${target}/source
 done
 
-cd $TMP
-
+# -----------------------------------------------------------------------------
 echo "$(date)  Creating symlinks"
-/usr/local/bin/symlinks
+# -----------------------------------------------------------------------------
 
+for f in $(find ${TMP} -name .symlinks); do
+  cd $(dirname $f)
+  cat $f | while read source destination; do
+    ln -sf ${source} ${destination} 2>/dev/null
+  done
+done
+cd ${SOURCE}
+
+# -----------------------------------------------------------------------------
 echo "$(date)  Obfuscating email addresses"
+# -----------------------------------------------------------------------------
+
 # This replaces all '@' in all html files with '&#64;'. We use '-type f'
 # because we want to exclude symlinks. Because 'sed -i' is a very expensive
 # operation, even if there is no replacement done anyway, we first limit the
 # files to operate on to those files that actually contain an '@'.
-find . -type f -name "*.html" | xargs grep -l '@' | xargs sed -i 's/@/\&#64;/g'
+find ${TMP} -type f -name "*.html" | xargs grep -l '@' | xargs sed -i 's/@/\&#64;/g'
 
-mv $DEST ${DEST}.old
-mv $TMP $DEST
+# -----------------------------------------------------------------------------
+echo "$(date)  Activating new output"
+# -----------------------------------------------------------------------------
+
+mv ${DEST} ${DEST}.old
+mv ${TMP} ${DEST}
 rm -rf ${DEST}.old
 
-cd $SOURCE
-
+# -----------------------------------------------------------------------------
 echo "$(date)  Generating translation logs"
+# -----------------------------------------------------------------------------
+
 tools/translation-log.sh ${DEST}/translations.log ${STATUS}
+
+# -----------------------------------------------------------------------------
 echo "$(date)  Build complete"
+# -----------------------------------------------------------------------------

7f7f15526f70f695191bbdbf872573a608004c2f
reinhard <reinhard@fsfe.org>
2008-02-21 19:14:03 +0000

    Hmmm....
    svn path=/trunk/; revision=10053

--- cgi-bin/weborder.pl
+++ cgi-bin/weborder.pl
@@ -20,7 +20,7 @@ my $amount = 0;
 # -----------------------------------------------------------------------------
 
 # Spam bots will be tempted to fill in this actually invisible field
-if (!$query->param("url")) {
+if (not $query->param("url")) {
   open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
   print MAIL "From: web\@fsfeurope.org\n";
   print MAIL "To: order\@fsfeurope.org\n";

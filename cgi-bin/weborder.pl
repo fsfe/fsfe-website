89e1fa82a6da0853849a870bc13fe024b979e140
reinhard <reinhard@fsfe.org>
2008-02-21 19:00:09 +0000

    weborder.pl now behaves like a successful order when spam is detected. This should confuse spammers.
    svn path=/trunk/; revision=10051

--- cgi-bin/weborder.pl
+++ cgi-bin/weborder.pl
@@ -10,11 +10,6 @@ if (!$query->param("_shipping")) {
   die "Invalid order, possibly a search engine robot.";
 }
 
-# Spam bots will be tempted to fill in this actually invisible field
-if ($query->param("url")) {
-  die "Invalid order, possibly spam.";
-}
-
 my $date = strftime "%Y-%m-%d", localtime;
 my $time = strftime "%s", localtime;
 my $reference = "order.$date." . substr $time, -5;
@@ -24,22 +19,25 @@ my $amount = 0;
 # Calculate amount and generate mail to office
 # -----------------------------------------------------------------------------
 
-open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
-print MAIL "From: web\@fsfeurope.org\n";
-print MAIL "To: order\@fsfeurope.org\n";
-print MAIL "Cc: mueller\@fsfeurope.org\n";
-print MAIL "Subject: Web order\n\n";
-print MAIL "$reference\n\n";
-foreach $name ($query->param) {
-  $value = $query->param($name);
-  if (not $name =~ /^_/ and $value) {
-    print MAIL "$name: $value\n";
-    $amount += $value * $query->param("_$name");
+# Spam bots will be tempted to fill in this actually invisible field
+if (!$query->param("url")) {
+  open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
+  print MAIL "From: web\@fsfeurope.org\n";
+  print MAIL "To: order\@fsfeurope.org\n";
+  print MAIL "Cc: mueller\@fsfeurope.org\n";
+  print MAIL "Subject: Web order\n\n";
+  print MAIL "$reference\n\n";
+  foreach $name ($query->param) {
+    $value = $query->param($name);
+    if (not $name =~ /^_/ and $value) {
+      print MAIL "$name: $value\n";
+      $amount += $value * $query->param("_$name");
+    }
   }
+  $amount = sprintf "%.2f", $amount;
+  print MAIL "Total amount: $amount\n";
+  close MAIL;
 }
-$amount = sprintf "%.2f", $amount;
-print MAIL "Total amount: $amount\n";
-close MAIL;
 
 # -----------------------------------------------------------------------------
 # Lead user to "thankyou" page

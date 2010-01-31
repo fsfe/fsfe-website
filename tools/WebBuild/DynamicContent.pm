5151e58e3f525dc159afde5c0a55d1a4b51fb375
ato <ato@fsfe.org>
2010-01-31 17:10:45 +0000

    Tools for generating dynamic content.
    svn path=/trunk/; revision=14712

---
+++ tools/WebBuild/DynamicContent.pm
@@ -0,0 +1,51 @@
+package WebBuild::DynamicContent;
+
+use strict;
+use warnings;
+
+use HTML::TreeBuilder::XPath;
+
+sub new {
+  my ($class, %args) = @_;
+  my $self = bless({}, $class);
+}
+
+sub content {
+  my ($self, $raw_output) = @_;
+  my $self->{raw_output} = $raw_output;
+
+  $self->transform;
+}
+
+sub transform {
+  my $self = shift;
+
+  my $output_parser = HTML::TreeBuilder::XPath->new;
+  $output_parser->parse($self->{raw_output});
+  my @output = $output_parser->disembowel;
+
+  my $document_parser = HTML::TreeBuilder::XPath->new;
+  $document_parser->parse_file("../boilerplate.en.html");
+
+  my $content = $document_parser->findnodes('//div[@id="content"]')->[0];
+  $content->findnodes("//p")->[0]->delete;
+  my $old_content = $content->detach_content;
+
+  foreach my $node (@output) {
+    $content->push_content($node);
+  }
+
+  $content->push_content($old_content);
+  $self->{output} = $tree->as_XML_indented;
+}
+
+sub render {
+  my $self = shift;
+
+  print "Content-type: text/html\n\n";
+  print $self->{output};
+  print "\n";
+}
+
+1;
+

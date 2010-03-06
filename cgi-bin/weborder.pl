f7e6e618daa846edf4d909eba87023dd3db464f9
reinhard <reinhard@fsfe.org>
2010-03-06 12:46:36 +0000

    I don't need a CC for all web orders any more.
    svn path=/trunk/; revision=15304

--- cgi-bin/weborder.pl
+++ cgi-bin/weborder.pl
@@ -29,7 +29,6 @@ if (not $query->param("url")) {
   open(MAIL, "|/usr/lib/sendmail -t -f mueller\@fsfeurope.org");
   print MAIL "From: web\@fsfeurope.org\n";
   print MAIL "To: order\@fsfeurope.org\n";
-  print MAIL "Cc: mueller\@fsfeurope.org\n";
   print MAIL "Subject: Web order $reference $buyer\n\n";
   print MAIL "$reference\n\n";
   foreach $name ($query->param) {

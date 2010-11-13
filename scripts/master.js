dc52198b6ea8a7c161d366ea034d896dd70cd373
ato <ato@fsfe.org>
2010-11-13 00:52:21 +0000

    Adding raw functionality for dismissing notice
    svn path=/branches/design/; revision=18304

---
+++ scripts/master.js
@@ -0,0 +1,14 @@
+/*
+
+/scripts/master.js -- Misc. script snippets needed for fsfe.org
+
+*/
+
+$(document).ready(function() {
+
+    // Close service notice when clicking dismiss button.
+    $("#service-notice .close a").click(function() {
+	alert("yes!");
+    });
+
+});

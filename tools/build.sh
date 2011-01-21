21b21f154a8469d221dbeff9b2b41063cee4288f
nicoulas <nicoulas@fsfe.org>
2011-01-21 16:32:15 +0000

    once again, mistakenly committed my local build.xx files - sorry, reverting
    svn path=/trunk/; revision=19393

--- tools/build.sh
+++ tools/build.sh
@@ -7,14 +7,14 @@
 # however, is done by the Perl script build.pl.
 # -----------------------------------------------------------------------------
 
-SOURCE=.
-DEST=../fsfe-web-out/home/www/html
-TMP=../fsfe-web-out/home/www/tmp.$$
-STATUS=../fsfe-web-out/var/www/web
+SOURCE=/home/www/fsfe
+DEST=/home/www/html
+TMP=/home/www/tmp.$$
+STATUS=/var/www/web
 ALARM_LOCKFILE=alarm_lockfile
 MAKEFILE_PL=${SOURCE}/Makefile.PL
-SVNUPOUTFILE=../fsfe-web-out/tmp/fsfe-svnup-out
-SVNUPERRFILE=../fsfe-web-out/tmp/fsfe-svnup-err
+SVNUPOUTFILE=/tmp/fsfe-svnup-out
+SVNUPERRFILE=/tmp/fsfe-svnup-err
 
 # Since we must grep for svn output messages,
 # let's ensure we get English messages
@@ -41,7 +41,7 @@ if [[ -n "$BUILD_STARTED" && "10#${BUILD_STARTED}" -gt 30 && ! -f ${STATUS}/${AL
 fi
 
 # Redirect output
-#exec 1> ${STATUS}/status.txt 2>&1
+exec 1> ${STATUS}/status.txt 2>&1
 
 # If some build script is already running, don't run it.
 if ps -C "build-df.sh,build-test.sh,build.sh" -o pid= | grep -q -v "$$"; then
@@ -65,7 +65,7 @@ echo "$(date)  Updating source files from SVN."
 # Since the "svn update" exit status cannot be trusted, and "svn update -q" is
 # always quiet, we have to test the output of "svn update" (ignoring the final
 # "At revision" line) and check for any output lines
-#svn --non-interactive update 2>${SVNUPERRFILE} | grep -v 'At revision' >${SVNUPOUTFILE}
+svn --non-interactive update 2>${SVNUPERRFILE} | grep -v 'At revision' >${SVNUPOUTFILE}
 cat ${SVNUPOUTFILE}
 
 # If "svn update" wrote anything to standard error, exit

077238c4ae48f3c4b1ce588bd9e297daae096900
reinhard <reinhard@fsfe.org>
2011-06-24 15:11:33 +0000

    More work on regular donations.
    svn path=/trunk/; revision=20743

--- cgi-bin/donate-new.pl
+++ cgi-bin/donate-new.pl
@@ -20,7 +20,13 @@ my $anonymous = $query->param("anonymous");
 my $language = $query->param("language");
 my $text = $query->param("text");
 
-my $amount100 = $amount * 100;
+my $amount100 = 0;
+my $subamount100 = 0;
+if ($period == "o") {
+  $amount100 = $amount * 100;
+} else {
+  $subamount100 = $amount * 100;
+}
 
 my $date = strftime("%Y-%m-%d", localtime);
 my $time = strftime("%s", localtime);
@@ -30,6 +36,13 @@ if ($anonymous) {
 } else {
   $reference = "donation.$date." . substr($time, -5);
 }
+if ($period != "o") $reference .= ".$period";
+
+my $months = 0;
+if ($period == "m") $months = 1;
+if ($period == "y") $months = 12;
+my $day = substr($date, -2)
+
 my $lang = substr($language, 0, 2);
 
 # -----------------------------------------------------------------------------
@@ -51,7 +64,19 @@ while (<TEMPLATE>) {
         "LANGUAGE=$language$passphrase" .
         "ORDERID=$reference$passphrase" .
         "PMLISTTYPE=2$passphrase" .
-        "PSPID=40F00871$passphrase" .
+        "PSPID=40F00871$passphrase";
+    if ($period != "o") $shastring .=
+        "SUB_AMOUNT=$subamount100$passphrase" .
+        "SUB_COM=$text$passphrase" .
+        "SUB_ENDDATE=2099-12-31$passphrase" .
+        "SUB_ORDERID=$reference$passphrase" .
+        "SUB_PERIOD_MOMENT=$day$passphrase" .
+        "SUB_PERIOD_NUMBER=$months$passphrase" .
+        "SUB_PERIOD_UNIT=m$passphrase" .
+        "SUB_STARTDATE=$date$passphrase" .
+        "SUB_STATUS=1$passphrase" .
+        "SUBSCRIPTION_ID=$reference$passphrase";
+    $shastring .= 
         "TP=http://fsfe.org/donate/tmpl-concardis.$lang.html$passphrase";
     my $shasum = uc(sha1_hex($shastring));
     print "    <form name=\"donate\" action=\"https://secure.payengine.de/ncol/prod/orderstandard.asp\" method=\"post\">\n";
@@ -62,6 +87,19 @@ while (<TEMPLATE>) {
     print "      <input type=\"hidden\" name=\"amount\"       value=\"$amount100\"/>\n";
     print "      <input type=\"hidden\" name=\"currency\"     value=\"EUR\"/>\n";
     print "      <input type=\"hidden\" name=\"language\"     value=\"$language\"/>\n";
+    if ($period != "o") {
+      print "      <!-- subscription parameters -->\n";
+      print "      <input type=\"hidden\" name=\"SUBSCRIPTION_ID\"   value=\"$reference\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_ORDERID\"       value=\"$reference\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_COM\"           value=\"$text\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_AMOUNT\"        value=\"$subamount100\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_PERIOD_UNIT\"   value=\"m\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_PERIOD_NUMBER\" value=\"$months\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_PERIOD_MOMENT\" value=\"$day\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_STARTDATE\"     value=\"$date\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_ENDDATE\"       value=\"2099-12-31\"/>\n";
+      print "      <input type=\"hidden\" name=\"SUB_STATUS\"        value=\"1\"/>\n";
+    }
     print "      <!-- interface template -->\n";
     print "      <input type=\"hidden\" name=\"TP\"           value=\"http://fsfe.org/donate/tmpl-concardis.$lang.html\"/>\n";
     print "      <input type=\"hidden\" name=\"PMListType\"   value=\"2\"/>\n";

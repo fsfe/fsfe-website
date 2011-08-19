e64c47fe27c2d9ce7e17c3f865a51346280fbacb
gollo <gollo@fsfe.org>
2011-08-19 18:47:39 +0000

    2011-08-19  Martin Gollowitzer  <gollo@fsfe.org>
    	* fixing a show-stopper
    
    svn path=/branches/test/; revision=20962

--- cgi-bin/event-registration.pl
+++ cgi-bin/event-registration.pl
@@ -64,7 +64,7 @@ my $boundary = "NextPart$reference";
 open(MAIL, "|/usr/lib/sendmail -t -f web\@fsfeurope.org");
 print MAIL "From: web\@fsfeurope.org\n";
 print MAIL "To: $to1\@fsfeurope.org\n";
-print MAIL "Cc: $email";
+print MAIL "Cc: $email\n";
 print MAIL "Subject: $subject\n";
 print MAIL "Mime-Version: 1.0\n";
 print MAIL "Content-Type: multipart/mixed; boundary=$boundary\n";

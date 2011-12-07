04631348af7e7933146cb5b931af85da6e03de00
ato <ato@fsfe.org>
2011-12-07 15:11:31 +0000

    More debugging
    svn path=/branches/test/; revision=21763

--- tools/build-test.pl
+++ tools/build-test.pl
@@ -450,10 +450,6 @@ sub process {
       	$subsite_stylesheet = $xslt_parser->parse_stylesheet($subsite_style_doc);
       }
 
-      if ($subsite_stylesheet) {
-	print "Subsite stylesheet loaded!\n";
-      }
-
       #
       # Here begins automated magic for those pages which we need to
       # assemble other sets of informations for first (automatically
@@ -722,8 +718,10 @@ sub process {
 
       unless ($opts{n}) {
 	if ($subsite_stylesheet) {
+	  print "Outputting using subsite stylesheet: $dir/$file.$lang.html\n";
 	  $subsite_stylesheet->output_file($results, "$opts{o}/$dir/$file.$lang.html");
 	} else {
+	  print "Outputting using global stylesheet: $dir/$file.$lang.html\n";
 	  $global_stylesheet->output_file($results, "$opts{o}/$dir/$file.$lang.html");
 	}
       }

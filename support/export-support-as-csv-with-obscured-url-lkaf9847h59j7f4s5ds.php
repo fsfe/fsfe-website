55be438622e39c5994f75be9e54bc77a7082116c
otto <otto@fsfe.org>
2012-04-25 09:36:42 +0000

    Fixed CSS error for support pages.
    svn path=/trunk/; revision=23159

---
+++ support/export-support-as-csv-with-obscured-url-lkaf9847h59j7f4s5ds.php
@@ -0,0 +1,23 @@
+<?php
+// This file is temporary - now I just need so see the db contents 
+// to develop email confirmation function
+
+$db = new PDO("sqlite:../../../db/support.sqlite");
+$query = $db->query("select * from t1");
+$first_row = true;
+while ($row = $query->fetch(PDO::FETCH_ASSOC))
+{
+  if ($first_row)
+  {
+    // I'm not sure how to get the field names using a PDO method but
+    // we can use the first row's (or any row's) key values as these
+    // are the field names.
+    $first_row = false;
+    $number_of_fields = count($row);
+    $field_names = array_keys($row);
+    $first_field_name = $field_names[0];
+  }
+  // do stuff here with the row
+  print_r($row);
+}
+?>

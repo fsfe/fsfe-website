f7ae18e43eed9e2767fdcd539d9665d0a04bf15d
otto <otto@fsfe.org>
2012-06-25 22:15:33 +0000

    Track also supporter confirmations in Piwik
    svn path=/trunk/; revision=23624

--- support/confirm.php
+++ support/confirm.php
@@ -47,7 +47,19 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
     echo "<h1>Hello ".$row['firstname']." ".$row['lastname']."</h1>
     <p>Your address ".$row['email']." was confirmed ".$row['confirmed'].".</p>
     <p>Thank you for your support to the <a href='http://fsfe.org/'>FSFE</a>!</p>";
-}
+    
+    echo '
+    <script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.fsfe.org/" : "http://piwik.fsfe.org/");
+	document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+	</script><script type="text/javascript">
+	try {
+	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 4);
+	piwikTracker.trackPageView();
+	piwikTracker.enableLinkTracking();
+	} catch( err ) {}
+	</script><noscript><p><img src="http://piwik.fsfe.org/piwik.php?idsite=4" style="border:0" alt=""></p></noscript>
+    ';
 
 if ($found == False) {
     echo "No address was confirmed. Please sign up again.";

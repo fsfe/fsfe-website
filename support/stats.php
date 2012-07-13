5f8df0cf55ee151b9c4e79615411352aaa783d25
otto <otto@fsfe.org>
2012-07-13 19:01:18 +0000

    Added initial version of supporter stats page.
    svn path=/trunk/; revision=23793

---
+++ support/stats.php
@@ -0,0 +1,38 @@
+<pre>
+<?php
+// report errors
+/*
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+*/
+
+try {
+    // open the database
+	$db = new PDO( 'sqlite:../../../db/support.sqlite' ); 
+}
+catch(PDOException $e) {
+	print 'Error while connecting to Database: '.$e->getMessage();
+}
+
+try {
+	// check data
+	$query = $db->prepare("SELECT COUNT(*) FROM t1 GROUP BY country");
+	$query->execute();
+}
+catch(PDOException $e) {
+	print "Database Error: \n";
+	print_r($db->errorInfo());
+}
+
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    // true if at least one row to return
+
+    print_r($row);
+
+}
+
+// close the database connection
+$db = NULL;
+
+?>
+</pre>

18e7aae0a5c72fa34c05e690fb4e241a2570d7a6
otto <otto@fsfe.org>
2012-08-23 11:18:50 +0000

    Clean up supporter database from testing entries.
    svn path=/trunk/; revision=24084

--- support/export-support-as-csv-with-obscured-url-lkaf9847h59j7f4s5ds.php
+++ support/export-support-as-csv-with-obscured-url-lkaf9847h59j7f4s5ds.php
@@ -6,6 +6,43 @@
 
 $db = new PDO("sqlite:../../../db/support.sqlite");
 
+$query = $db->query("delete from t1 where id='2'");
+$query = $db->query("delete from t1 where id='3'");
+$query = $db->query("delete from t1 where id='4'");
+$query = $db->query("delete from t1 where id='5'");
+$query = $db->query("delete from t1 where id='6'");
+$query = $db->query("delete from t1 where id='7'");
+$query = $db->query("delete from t1 where id='9'");
+$query = $db->query("delete from t1 where id='10'");
+$query = $db->query("delete from t1 where id='13'");
+$query = $db->query("delete from t1 where id='15'");
+$query = $db->query("delete from t1 where id='16'");
+$query = $db->query("delete from t1 where id='17'");
+$query = $db->query("delete from t1 where id='18'");
+$query = $db->query("delete from t1 where id='19'");
+$query = $db->query("delete from t1 where id='25'");
+$query = $db->query("delete from t1 where id='74'");
+$query = $db->query("delete from t1 where id='82'");
+$query = $db->query("delete from t1 where id='84'");
+$query = $db->query("delete from t1 where id='85'");
+$query = $db->query("delete from t1 where id='86'");
+$query = $db->query("delete from t1 where id='87'");
+$query = $db->query("delete from t1 where id='88'");
+$query = $db->query("delete from t1 where id='89'");
+$query = $db->query("delete from t1 where id='90'");
+$query = $db->query("delete from t1 where id='91'");
+$query = $db->query("delete from t1 where id='136'");
+$query = $db->query("delete from t1 where id='137'");
+$query = $db->query("delete from t1 where id='138'");
+$query = $db->query("delete from t1 where id='139'");
+$query = $db->query("delete from t1 where id='162'");
+$query = $db->query("delete from t1 where id='163'");
+$query = $db->query("delete from t1 where id='175'");
+$query = $db->query("delete from t1 where id='182'");
+$query = $db->query("delete from t1 where id='183'");
+$query = $db->query("delete from t1 where id='184'");
+$query = $db->query("delete from t1 where id='185'");
+
 //$query = $db->query("alter table t1 drop column signed");
 // not possible to drop columns in LiteSQL
 

3a0cd54cad4cbcb74238fac8e7fbc7f5801615a1
leobaillard <leobaillard@fsfe.org>
2012-10-16 09:30:55 +0000

    Update the support portal to reflect the live version.
    svn path=/branches/test/; revision=24667

---
+++ support/export-support-as-csv-with-obscured-url-lkaf9847h59j7f4s5ds.php
@@ -0,0 +1,42 @@
+<?php
+// This file is temporary - now I just need so see the db contents 
+// to develop email confirmation function
+
+die("This file is for debugging only.");
+
+$db = new PDO("sqlite:../../../db/support.sqlite");
+
+//$query = $db->query("alter table t1 drop column signed");
+// not possible to drop columns in LiteSQL
+
+$query = $db->query("select * from t1");
+
+header("Content-type: text/csv");  
+header("Cache-Control: no-store, no-cache");  
+header('Content-Disposition: attachment; filename="supporters.csv"');
+
+$outstream = fopen("php://output",'w'); 
+
+$first_row = true;
+while ($row = $query->fetch(PDO::FETCH_ASSOC))
+{
+  if ($first_row)
+  {
+    // I'm not sure how to get the field names using a PDO method but
+    // we can use the first row's (or any row's) key values as these
+    // are the field names.
+    $first_row = false;
+    $number_of_fields = count($row);
+    $field_names = array_keys($row);
+    fputcsv($outstream, $field_names, ',', '"');  
+  }
+  // do stuff here with the row
+  fputcsv($outstream, $row, ',', '"');  
+}
+
+fclose($outstream);
+
+// close the database connection
+$db = NULL;
+
+?>
---
+++ support/remind-unconfirmed-with-obscured-url-sdkfsy3i4j5skd354kmds4w.php
@@ -0,0 +1,90 @@
+<?php
+header("Content-Type: text/plain")
+?>
+Sending reminders to supporters who's e-mail is still unconfirmed:
+<?
+/*
+ini_set( "display_errors","1" );
+ERROR_REPORTING( E_ALL) ;
+*/
+
+$timestamp = date('Y-m-d H:i:s');
+
+try {
+    //open the database
+	$db = new PDO( 'sqlite:../../../db/support.sqlite' ); 
+}
+catch(PDOException $e) {
+	print 'Error while connecting to Database: '.$e->getMessage();
+}
+
+// get all unconfirmed rows
+try {
+	$query = $db->prepare("SELECT * FROM t1 WHERE confirmed is NULL 
+	    AND time > '".date('Y-m-d', time()-60*60*24*30)." 00:00:00'"); // restrict to rows younger than a month
+	$query->execute();
+}
+catch(PDOException $e) {
+    print "Database Error: \n";
+    print_r($db->errorInfo());
+}
+
+
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    echo "\n".$row['email'].
+    " signed up ".$row['time'].
+    ", confirmed: ".$row['confirmed']."\n".
+    "    reminder 1: ".$row['reminder1'].
+    " 2: ".$row['reminder2'].
+    " 3: ".$row['reminder3']."\n";
+    
+    $two_days_ago = date('Y-m-d', time()-60*60*24*2)." 00:00:00";
+    
+    // send only one reminder
+    // don't send reminders more frequent than every third day
+    if ($row['reminder1'] == '' && $row['time'] < $two_days_ago) {
+        send_reminder("1", $row);
+    } elseif ($row['reminder2'] == '' && $row['reminder1'] != '' && $row['reminder1'] < $two_days_ago) {
+        send_reminder("2", $row);
+    } elseif ($row['reminder3'] == '' && $row['reminder2'] != '' && $row['reminder2'] < $two_days_ago) {
+        send_reminder("3", $row);
+    } // else do nothing
+}
+
+
+// close the database connection
+$db = NULL;
+
+
+function send_reminder($reminder_number, $row) {
+    GLOBAL $db, $timestamp;
+    $secret = $row['secret'];
+
+    if (file_exists('template-email-confirm.'. $row['lang'] .'.inc')) {
+        require('template-email-confirm.'. $row['lang'] .'.inc');
+    } else {    
+        require('template-email-confirm.en.inc');
+    }
+    
+    $to      = $row['email']; 
+    $headers = 'From: "FSFE" <office@fsfe.org>' . "\r\n";
+    mail($to, $subject, $message, $headers);
+
+    echo "    => Sent reminder number ".$reminder_number." to ".$row['email']."\n";
+    
+    try {
+        $query = $db->prepare("UPDATE t1 SET
+            reminder".$reminder_number." = '". $timestamp ."'
+            where secret='". $row['secret'] ."'");
+        $query->execute();
+    }
+    catch(PDOException $e) {
+        print "Database Error: \n";
+        print_r($db->errorInfo());
+        return false;
+    }
+    
+    return true;
+}
+
+?>

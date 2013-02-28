00a2f75e7c3b58cf60402c0f66711eb697aff3ca
cri <cri@fsfe.org>
2013-02-28 12:59:49 +0000

    Porting improvements to status page from test branch
    svn path=/trunk/; revision=25364

--- tools/status.php
+++ tools/status.php
@@ -1,49 +1,65 @@
+<?php
+
+/*
+ * Implement the page http://status.fsfe.org/web/
+ */
+
+echo '
 <!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-	  <title>Build script output</title>
-	</head>
-	<body>
+<head>
+  <meta charset="utf-8" />
+  <title>Build script output - production instance</title>
+</head>
+<body>
 
-		<h1>Build script output</h1>
+<h1>Build script output - production instance</h1>
+<p><a href="http://status.fsfe.org/">« Back to <em>Web server status</em></a></p>
+';
 
-		<p>
-			<a href="http://status.fsfe.org/">« Back to <em>Web server 
-				status</em></a>
-		</p>
+# If there is a currently running build, show its log
+if ( file_exists('status.txt') ) {
+  echo '
+  <h2>Currently running build</h2>
+  <p><pre>
+';
+  $status = file_get_contents("status.txt");
+  echo htmlspecialchars($status);
+  echo '
+  </pre></p>
+  ';
+}
 
+# If the last build failed or there were no changes to SVN, show its log
+if ( file_exists('status-attempted.txt') ) {
+  echo '
+  <h2>Last attempted build</h2>
+  <p><pre>
+';
+  $status = file_get_contents("status-attempted.txt");
+  echo htmlspecialchars($status);
+  echo '
+  </pre></p>
+  ';
+}
 
-		<h2>Last attempted or currently running build</h2>
-
-		<p>
-			<pre>
-<?php 
-$status = file_get_contents("status.txt");
-echo htmlspecialchars($status);
-?>
-			</pre>
-		</p>
-
-
-		<h2>Last finished build</h2>
-
-		<p>
-			<pre>
-<?php 
+# Always show the log of last finished build
+echo '
+<h2>Last finished build</h2>
+<p><pre>
+';
 $statusfinished = file_get_contents("status-finished.txt");
 echo htmlspecialchars($statusfinished);
+echo '
+</pre></p>
+
+<h2>Other tools</h2>
+
+<ul>
+  <li><a href="./status-log.txt">Previously finished builds</a></li>
+  <li><a href="https://trac.fsfe.org/fsfe-web/browser/trunk">SVN web browser</a></li>
+</ul>
+
+</body>
+</html>';
 ?>
-			</pre>
-		</p>
-
-
-		<h2>Other tools</h2>
-
-		<ul>
-			<li><a href="./status-log.txt">Previously finished builds</a></li>
-			<li><a href="https://trac.fsfe.org/fsfe-web/browser/trunk">SVN web browser</a></li>
-		</ul>
-	</body>
-</html>
-

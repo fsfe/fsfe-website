a656b950d73af874f6be636f7d4f6c88fdf30ce1
mk <mk@fsfe.org>
2014-02-06 15:42:33 +0000

    fixing mail alias
    svn path=/trunk/; revision=27918

--- cgi-bin/expense-request.pl
+++ cgi-bin/expense-request.pl
@@ -150,7 +150,7 @@ $subject .= ")";
 
 my $boundary = "NextPart$reference";
 
-my $replyto = "finances\@fsfeurope.org, $who\@fsfeurope.org, $to1\@fsfeurope.org";
+my $replyto = "finance\@fsfeurope.org, $who\@fsfeurope.org, $to1\@fsfeurope.org";
 if ($budget2 ne "NONE") {
   $replyto .= ", $to2\@fsfeurope.org";
 }

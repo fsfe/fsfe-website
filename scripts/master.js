f7c794b6b920360761918b8b7bf997d574346bd1
hugo <hugo@fsfe.org>
2014-10-02 17:52:48 +0000

    use alert.js from boostrap instead of custom code from the old design
    svn path=/branches/test/; revision=29463

--- scripts/master.js
+++ scripts/master.js
@@ -9,8 +9,8 @@
 $(document).ready(function() {
 
     // Close service notice on dismiss button.
-    $("#service-notice .close a").click(function() {
-        $("#service-notice").hide();
+    $(".alert a.close").click(function() {
+        $(".alert").hide();
     });
 
 });

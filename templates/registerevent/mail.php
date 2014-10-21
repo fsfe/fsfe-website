dc918ded3f1dc963ae748bb36d48b9c7ca98feac
pavi <pavi@fsfe.org>
2014-10-21 12:51:24 +0000

    changed mail template
    svn path=/branches/test/; revision=29697

--- templates/registerevent/mail.php
+++ templates/registerevent/mail.php
@@ -7,22 +7,17 @@ Content-Disposition: inline
  
 Hi, 
  
-<?=$name?> registered an event on http://fsfe.org/community/tools/register-event 
-Below is a list of the information they gave.
-Name: <?=$name?> 
-Email: <?=$email?> 
-Groupname: <?=$groupname?>
+You have registered an event on http://fsfe.org/community/tools/register-event 
+Below is the list of the information you gave.
+	Name: <?=$name?> 
+	Email: <?=$email?> 
+	Groupname: <?=$groupname?>
 
-Location: <?=$location?>
+	Location: <?=$location?>
  
-The files you need to commit to the svn are attached. 
- 
-Please reply to the person who registerd saying "Hi" and 
-"Thank You". You should also check if the given information 
-is valid. 
- 
-If it is not, please correct it in the xml file. 
- 
+You find all other information in the xml file attached. It will be uploaded
+in the next 24 hours. If not, or in case you like to change some information,
+please contact fellowship@fsfeurope.org
  
 Thanks,
 your website

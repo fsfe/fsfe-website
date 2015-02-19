7f72961c1c91288f50255d7fb5c2647950bdb5d4
eal <eal@fsfe.org>
2015-02-19 11:22:04 +0000

    optimizing event registration receivers
    svn path=/trunk/; revision=30644

--- cgi-bin/registerevent.php
+++ cgi-bin/registerevent.php
@@ -59,9 +59,10 @@ function send_registration_mail() {
 
 	$message = eval_template('registerevent/mail.php', $data);
 
-	$to = $_POST['email'].",fellowship@fsfeurope.org";
+	$to = $_POST['email'].",eal@fsfe.org,fellowship@fsfeurope.org";
 	$subject = "event registration: " . $_POST['name'];
 	$headers = "From: no-reply@fsfe.org\n"
+		. "CC: eal@3rik.cc\n"
 		. "MIME-Version: 1.0\n"
 		. "Content-Type: multipart/mixed; boundary=boundary";
 

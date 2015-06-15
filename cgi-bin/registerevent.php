34e708fd85f7d7ca9cbf9319a310baffc96b3494
mjammes <mjammes@fsfe.org>
2015-06-15 14:29:20 +0000

    testing the new event registration page
    svn path=/branches/test/; revision=31369

--- cgi-bin/registerevent.php
+++ cgi-bin/registerevent.php
@@ -36,7 +36,7 @@ function get_mime_type($path) {
 
 function eval_date($date) {
 	$dt = date_parse($date);
-	return (!$dt['errors'] && $dt['year']);
+	return ($dt['year']);
 }
 
 
@@ -72,7 +72,8 @@ function send_registration_mail() {
 	return $data['img_error'];
 }
 
-if ( isset($_POST['register_event']) && empty($_POST['spam']) && preg_match([0-9]{4}, eval_date($_POST['startdate'])) && (empty($_POST['enddate']) || preg_match([0-9]{4}, eval_date($_POST['enddate']))) ) {
+
+if ( isset($_POST['register_event']) && empty($_POST['spam']) && preg_match([0-9]{4}, eval_date($_POST['startdate'])) ) {
 	$error = send_registration_mail();
 
 	echo eval_xml_template('registerevent/success.en.html', array(

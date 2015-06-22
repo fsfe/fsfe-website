7e139c8cdfa8c02aa35b60fcffb709011337941a
mjammes <mjammes@fsfe.org>
2015-06-22 11:55:24 +0000

    Testing pregmatch for new registration event script
    svn path=/branches/test/; revision=31435

--- cgi-bin/registerevent.php
+++ cgi-bin/registerevent.php
@@ -36,7 +36,7 @@ function get_mime_type($path) {
 
 function eval_date($date) {
 	$dt = date_parse($date);
-	return (!$dt['errors'] && $dt['year'] && preg_match("#^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$#", $date));
+	return (!$dt['errors'] && $dt['year'] && preg_match("#^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$#", $date) === 1);
 }
 
 

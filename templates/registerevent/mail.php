8ab8bd5db4422f48481eaa69bebe0a64ac5f6f2b
eal <eal@fsfe.org>
2015-10-05 15:14:03 +0000

    improving mail-text that is send
    svn path=/trunk/; revision=32022

--- templates/registerevent/mail.php
+++ templates/registerevent/mail.php
@@ -7,7 +7,8 @@ Content-Disposition: inline
  
 Hi, 
  
-You have registered an event on http://fsfe.org/community/tools/register-event 
+You have registered an event on http://fsfe.org/community/tools/eventregistration
+
 Below is the list of the information you gave.
 	Name: <?=$name?> 
 	Email: <?=$email?> 
@@ -15,7 +16,7 @@ Below is the list of the information you gave.
 	Location: <?=$city?>, <?=$country?>
  
 You find all other information in the xml file attached. It will be uploaded
-in the next 24 hours. If not, or in case you like to change some information,
+in the next 24 hours. If you like to withdraw your event or in case you like to change some information,
 please contact fellowship@fsfeurope.org
  
 Thanks,

5d05e372a0708cf165a14ec57f43c48df85b703b
max.mehl <max.mehl@fsfe.org>
2016-06-13 09:19:28 +0000

    add license notice; improve some wording
    svn path=/trunk/; revision=33502

--- cgi-bin/perdiem.php
+++ cgi-bin/perdiem.php
@@ -1,4 +1,30 @@
 <?php
+/***********************************************************************
+*  Copyright (C) 2016 Max Mehl <max.mehl [at] fsfe [dot] org> for FSFE e.V.
+************************************************************************
+*  
+*  This program is free software: you can redistribute it and/or modify 
+*  it under the terms of the GNU Affero General Public License as 
+*  published by the Free Software Foundation, either version 3 of the 
+*  License, or (at your option) any later version.
+*  
+*  This program is distributed in the hope that it will be useful, 
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+*  GNU Affero General Public License for more details.
+*  
+*  You should have received a copy of the GNU Affero General Public 
+*  License along with this program.  If not, see 
+*  <http://www.gnu.org/licenses/>.
+*  
+************************************************************************
+*  
+*  This file receives input from /internal/pd.en.(x)html and calculates per 
+*  diem charges for reimbursement claims. The amounts can be defined in 
+*  the XHTML file. For the output of the data, it uses 
+*  /internal/pd-result.en.(x)html as a template.
+*  
+***********************************************************************/
 
 $output = '';
 

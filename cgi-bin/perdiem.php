96b38e31e35777110a7c2c37ac19967328a037e3
max.mehl <max.mehl@fsfe.org>
2017-01-16 20:54:30 +0000

    add spreadsheet copy/paste feature to per diem calculator; code improvements
    svn path=/trunk/; revision=34939

--- cgi-bin/perdiem.php
+++ cgi-bin/perdiem.php
@@ -61,6 +61,7 @@ $epd = explode('/', $epd);  // separate at "/"
 $epd_trav = $epd[0];  // first half
 $epd_full = $epd[1];  // second hald
 
+$output .= "<h2>Detailled overview of per diems</h2>";
 $output .= "<p>Travel day(s): " . $epd_trav . $cur . " per day <br />";
 $output .= "Full day(s): " . $epd_full . $cur . " per day <br />";
 $output .= "Own country: " . $home . " <br />";
@@ -77,24 +78,24 @@ $output .= '<table class="table table-striped">
     <th>Your total reimbursement</th>
   </tr>';
 
-$days = array('out', 1, 2, 3, 4, 5, 6, 7, 'return');
+$days = array('out', 1, 2, 3, 4, 5, 6, 7, 'return'); // define day range
 
 // set variables
 $r_total = 0;   // total reimbursement
 
-foreach ($days as &$day) {  // calculate for each day
-  $use = $_POST['use'][$day];
-  $date = $_POST['date'][$day];
-  $break = $_POST['break'][$day];
-  $lunch = $_POST['lunch'][$day];
-  $dinner = $_POST['dinner'][$day];
+foreach ($days as &$d) {  // calculate for each day
+  $use[$d] = $_POST['use'][$d];
+  $date[$d] = $_POST['date'][$d];
+  $break[$d] = $_POST['break'][$d];
+  $lunch[$d] = $_POST['lunch'][$d];
+  $dinner[$d] = $_POST['dinner'][$d];
   
   // set variables
-  $r_day = 0;   // reimbursement for this day
+  $r_day[$d] = 0;   // reimbursement for this day
   
   
-  if ($use === 'yes') { // only calculate if checkbox has been activated (day in use)
-    if ($day === 'out' || $day === 'return') {  // set amount of € for travel or full day
+  if ($use[$d] === 'yes') { // only calculate if checkbox has been activated (day in use)
+    if ($d === 'out' || $d === 'return') {  // set amount of € for travel or full day
       $eur = $epd_trav;   // total reimburseable amount for this day
       $desc = " (travel)";
     } else {
@@ -105,33 +106,33 @@ foreach ($days as &$day) {  // calculate for each day
     $output .= "<tr>";
     
     // date
-    if ($date === '' ) {
-      $date = "Day " . $day;
+    if ($date[$d] === '' ) {
+      $date[$d] = "Day " . $d;
     }
-    $output .= "<td>" . $date . $desc . "</td>";
+    $output .= "<td>" . $date[$d] . $desc . "</td>";
     
     // breakfast ($r_b)
-    if ($break === "yes") {
+    if ($break[$d] === "yes") {
       $r_b = $eur * $c_b;
-      $r_day = $r_day + $r_b;
+      $r_day[$d] = $r_day[$d] + $r_b;
       $output .= "<td>yes (" . $r_b . $cur . ")</td>";
     } else {
       $output .= "<td>no</td>";
     }
     
     // lunch ($r_l)
-    if ($lunch === "yes") {
+    if ($lunch[$d] === "yes") {
       $r_l = $eur * $c_l;
-      $r_day = $r_day + $r_l;
+      $r_day[$d] = $r_day[$d] + $r_l;
       $output .= "<td>yes (" . $r_l . $cur . ")</td>";
     } else {
       $output .= "<td>no</td>";
     }
     
     // breakfast ($r_d)
-    if ($dinner === "yes") {
+    if ($dinner[$d] === "yes") {
       $r_d = $eur * $c_d;
-      $r_day = $r_day + $r_d;
+      $r_day[$d] = $r_day[$d] + $r_d;
       $output .= "<td>yes (" . $r_d . $cur . ")</td>";
     } else {
       $output .= "<td>no</td>";
@@ -139,23 +140,84 @@ foreach ($days as &$day) {  // calculate for each day
     
     // flat rate
     $r_flat = $eur * $c_flat;
-    $r_day = $r_day + $r_flat;
+    $r_day[$d] = $r_day[$d] + $r_flat;
     $output .= "<td>" . $r_flat . $cur . "</td>";
     
     // reimbursement for the single day
-    $output .= "<td>" . $r_day . $cur . "</td>";
-    $r_total = $r_total + $r_day;
+    $output .= "<td>" . $r_day[$d] . $cur . "</td>";
+    $r_total = $r_total + $r_day[$d];
     
     // close row (day)
     $output .= "</tr>";
   
   } // if day is used
-}
+} // foreach
 
 $output .= "<tr><td></td><td></td><td></td><td></td><td></td>";
 $output .= "<td><strong>Total per diem: " . $r_total . $cur . "</strong></td></tr></table>";
 
-//------------------------------------
+/* ------------------------------------
+* --- PRINT FOR COPY IN SPREADSHEET ---
+* -----------------------------------*/
+$output .= "<hr />";
+$output .= "<h2>Spreadsheet copy&amp;paste</h2>";
+$output .= "<p>The following section can be copied to your Reimbursement Claim spreadsheet. Please mark and copy the content of the table (except the headings), and paste it in your spreadsheet application.<br /><em>Hint: In LibreOffice, press Ctrl+Shift+V in order to paste it as unformatted text instead of HTML code. This avoids conflicting formatting.</em></p>";
+
+
+$output .= '<table class="table">';
+$output .= '<tr><th>Employee name</th><th>Date</th><th>Amount (EUR)</th><th>Recipient Name</th><th>ER number</th><th>Catchphrase</th><th>Receipt / per diem</th><th>Remarks</th></tr>';
+foreach ($days as &$d) {  // calculate for each day
+  if ($use[$d] === 'yes') {
+    $rc_name = $_POST['rc_name'];
+    $rc_er = $_POST['rc_er'];
+    $rc_catch = $_POST['rc_catch'];
+    
+    if ($rc_name === '') { $rc_name = "YOUR-NAME"; }
+    if ($rc_er === '') { $rc_er = "ER-NUMBER"; }
+    if ($rc_catch === '') { $rc_catch = "CATCHPHRASE"; }
+    
+    $output .= "<tr>";
+    
+    $output .= "<td>" . $rc_name . "</td>";
+    $output .= "<td>" . $date[$d] . "</td>";
+    
+    $r_day[$d] = preg_replace("/\./", ",", $r_day[$d]);   // replace . by , in amount to make it compatible with the used spreadsheet template
+    $output .= "<td>" . $r_day[$d] . "</td>";
+    
+    $output .= "<td></td>";
+    $output .= "<td>" . $rc_er . "</td>";
+    $output .= "<td>" . $rc_catch . "</td>";
+    $output .= "<td>per diem</td>";
+    
+    if ($break[$d] === "yes") {
+      $remark[$d] = "breakfast+";
+    }
+    if ($lunch[$d] === "yes") {
+      $remark[$d] .= "lunch+";
+    }
+    if ($dinner[$d] === "yes") {
+      $remark[$d] .= "dinner";
+    }
+    if ($break[$d] != "yes" && $lunch[$d] != "yes" && $dinner[$d] != "yes") {
+      $remark[$d] = "nothing";
+    }
+    if ($break[$d] === "yes" && $lunch[$d] === "yes" && $dinner[$d] === "yes") {
+      $remark[$d] = "everything";
+    }
+    $remark[$d] = preg_replace("/\+$/", "", $remark[$d]);
+    $remark[$d] .= " self-paid";
+    
+    $output .= "<td>" . $remark[$d] . "</td>";
+    
+    $output .= "</tr>";
+  }
+
+}
+
+$output .= '</table>';
+
+
+// --- PRINT OUTPUT IN TEMPLATE FILE ---
 
 function replace_page($temp, $content){
     $vars = array(':RESULT:'=>$content);

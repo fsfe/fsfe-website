35cb77b11a446c87027719612ced167909aa5cda
Vincent Lequertier <vincent@fsfe.org>
2018-03-07 10:35:49 +0100

    Add anchor links to the translations status page

--- tools/make-outdated-list.sh
+++ tools/make-outdated-list.sh
@@ -4,12 +4,23 @@ cd /srv/www/fsfe.org_git
 nowlang=''
 yearago=`date +%s --date='1 year ago'`
 
-cat << _END_
+cat  > translations.html << _END_
 <html>
  <body>
-  <span style="color: red">Red entries</span> are pages where the original is newer than one year, and the translation is more than 3 months old. <span style="color: orange">Orange entries</span> are pages where the translation is older, but the original is older than one year.
+  <p><span style="color: red">Red entries</span> are pages where the original is newer than one year, and the translation is more than 3 months old. <span style="color: orange">Orange entries</span> are pages where the translation is older, but the original is older than one year.</p>
+  <p>Click on the links below to jump to a particular language</p>
 
 _END_
+
+find . -type f -iname "*\.en\.xhtml" | grep -v '^./[a-z][a-z]/\|^./news'|sed 's/\.[a-z][a-z]\.xhtml//'|sort|while read A; do
+   originaldate=`git log --pretty="%cd" --date=raw -1 $A.en.xhtml|cut -d' ' -f1`
+   for i in $A.[a-z][a-z].xhtml; do
+         lang_uniq=`echo $i|sed 's/.*\.\([a-z][a-z]\)\.xhtml/\1/'`
+         echo $lang_uniq
+     done
+ done|sort|uniq|while read lang_uniq; do
+   echo "<a href=#$lang_uniq>$lang_uniq</a>" >> translations.html
+done
 
 find . -type f -iname "*\.en\.xhtml" | grep -v '^./[a-z][a-z]/\|^./news'|sed 's/\.[a-z][a-z]\.xhtml//'|sort|while read A; do
    originaldate=`git log --pretty="%cd" --date=raw -1 $A.en.xhtml|cut -d' ' -f1`
@@ -27,11 +38,11 @@ done|sort -t' ' -k 1,1 -k 3nr,3 -k 5nr,5|\
 while read lang page originaldate trdate diff; do
   if [[ $nowlang != $lang ]]; then
     if [[ $nowlang != "" ]]; then
-     echo "</table>"
+     echo "</table>" >> translations.html
     fi
-    echo "<h1>Language: $lang</h1>"
-    echo "<table>"
-    echo "<tr><th>Page</th><th>Original date</th><th>Translation date</th><th>Age (in days)</th></tr>"
+    echo "<h1 id=\"$lang\">Language: $lang</h1>" >> translations.html
+    echo "<table>" >> translations.html
+    echo "<tr><th>Page</th><th>Original date</th><th>Translation date</th><th>Age (in days)</th></tr>" >> translations.html
     nowlang=$lang
   fi
   orig=`date +"%Y-%m-%d" --date="@$originaldate"`
@@ -45,10 +56,10 @@ while read lang page originaldate trdate diff; do
   else
     color=''
   fi
-  echo "<tr><td$color>$page</td><td>$orig</td><td>$tr</td><td$color>$diffdays</td></tr>"
+  echo "<tr><td$color>$page</td><td>$orig</td><td>$tr</td><td$color>$diffdays</td></tr>" >> translations.html
 
 done
 
-echo "</table>"
-echo "</body>"
-echo "</html>"
+echo "</table>" >> translations.html
+echo "</body>" >> translations.html
+echo "</html>" >> translations.html

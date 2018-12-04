8f9f61eb3b28a1dcc758a7396f07375bcc2f874b
max.mehl <max.mehl@fsfe.org>
2018-12-04 14:59:10 +0100

    slightly rewrite Thomas' script and add to drone's syntax check

---
+++ tools/check-non-en-frontpage.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# checks whether there non-EN items appear on the FSFE front-page, which is undesired
+
+DIR=$1
+
+# select all items which have the front-page tag
+exit=0
+for f in $( grep -R "<tag.*>front-page</tag>" --files-with-matches ${DIR}/2*/*.{xhtml,xml} )
+do
+	base="${f%.*}"      # file.xx
+	base="${base%.*}"   # file
+	sfx=${f##*.}        # xhtml
+	en="$base.en.$sfx"  # file.en.xhtml
+	if [[ ! -f "$en" ]]
+	then
+		echo "$f"         # echo file if it's not existent in an English version
+    exit=1
+	fi
+done
+
+exit $exit

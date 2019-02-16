f7bd3ce9620e51e6f7931d67b189e19e8d3d320a
Reinhard Müller <reinhard@fsfe.org>
2019-02-16 11:17:58 +0100

    Fix exit status in process_file in case of build error

--- build/process_file.sh
+++ build/process_file.sh
@@ -5,5 +5,7 @@ basedir="${0%/*}/.."
 
 . "$basedir/build/arguments.sh"
 
-[ "$command" = process_file ] && process_file "$workfile" "$processor" "$olang" \
-|| die "Urecognised command or no command given"
+case "$command" in
+  process_file) process_file "$workfile" "$processor" "$olang" ;;
+  *) die "Unrecognised command or no command given" ;;
+esac

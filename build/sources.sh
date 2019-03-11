ddd30b818863cce19cc890b4e59c7104ac2bba58
Reinhard Müller <reinhard@fsfe.org>
2019-03-11 16:52:57 +0100

    Avoid error message for builds without .xmllist file

--- build/sources.sh
+++ build/sources.sh
@@ -11,9 +11,13 @@ list_sources(){
   shortname="$1"
   lang="$2"
 
-  cat "`dirname ${shortname}`/.`basename ${shortname}`.xmllist" | while read base; do
-    echo "${basedir}/${base}".[a-z][a-z].xml "${basedir}/${base}".en.[x]ml "${basedir}/${base}.${lang}".[x]ml
-  done | sed -rn 's;^(.* )?([^ ]+\.[a-z]{2}\.xml).*$;\2;p'
+  list_file="`dirname ${shortname}`/.`basename ${shortname}`.xmllist"
+
+  if [ -f "${list_file}" ]; then
+    cat "${list_file}" | while read base; do
+      echo "${basedir}/${base}".[a-z][a-z].xml "${basedir}/${base}".en.[x]ml "${basedir}/${base}.${lang}".[x]ml
+    done | sed -rn 's;^(.* )?([^ ]+\.[a-z]{2}\.xml).*$;\2;p'
+  fi
 }
 
 auto_sources(){
@@ -27,6 +31,5 @@ auto_sources(){
     printf '\n### filename="%s" ###\n%s' "$source" "$(include_xml "$source")" 
   done \
   | sed -r ':X; N; $!bX;
-            s;\n### (filename="[^\n"]+") ###\n[^<]*(<![^>]+>[^<]*)*(<([^/>]+/)*([^/>]+))(/?>);\2\3 \1\6;g;
-           '
+            s;\n### (filename="[^\n"]+") ###\n[^<]*(<![^>]+>[^<]*)*(<([^/>]+/)*([^/>]+))(/?>);\2\3 \1\6;g;'
 }

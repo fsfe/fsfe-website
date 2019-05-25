e82eed33e9b4e1ce290fa37a9e88c24878278683
Michael Weimann <mail@michael-weimann.eu>
2019-05-25 12:38:44 +0200

    Implement frontend validation fo the donate amount #770

---
+++ scripts/spreadtheword.js
@@ -0,0 +1,37 @@
+// This file contains JavaScript for the "spreadtheword" page
+
+(function() {
+    // Validate the donation field.
+    // Shows a validation error if one of the following conditions match
+    // - the donate field is empty
+    // - the donate field value is below 10
+
+    var form = document.getElementById('orderpromo');
+    var donationAmountInput = document.getElementById('donate');
+    var donationErrorElement = document.getElementById('donate-error');
+    var donateGroup = document.getElementById('donate-group');
+
+    /**
+     * Sets the donate field to the error state.
+     */
+    function setDonateError() {
+        donateGroup.classList.add('has-error');
+        donationErrorElement.style.display = 'block';
+    }
+
+    form.addEventListener('submit', function (event) {
+        var value = donationAmountInput.value;
+
+        if (value) {
+            var parsedValue = parseInt(value, 10);
+
+            if (parsedValue < 10) {
+                setDonateError();
+                event.preventDefault();
+            }
+        } else {
+            setDonateError();
+            event.preventDefault();
+        }
+    });
+})();

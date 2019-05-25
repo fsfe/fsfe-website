86e2cc9ff3a9d065f735078d7652e7d43040e013
Reinhard Müller <reinhard@fsfe.org>
2019-05-25 16:50:58 +0200

    Remove character set conversion for XML files
    All XML files are now in UTF-8, thanks to #641. So no character set
    conversion is necessary any more. The removal of the conversion step
    should make the build process faster, especially for those pages with a
    lot of included XML files.

--- build/xmlfiles.sh
+++ build/xmlfiles.sh
@@ -2,37 +2,23 @@
 
 inc_xmlfiles=true
 
-unicat(){
-  # convert XML files to UTF-8
-  for file in "$@"; do
-    enc="$(sed -nr 'bA; :Q q; :A s:^.*<\?.*encoding="([^"]+)".*$:\1:p; tQ' "$file")"
-    iconv -f "${enc:-UTF-8}" -t "UTF-8" "$file"
-  done
-}
-
 include_xml(){
   # include second level elements of a given XML file
   # this emulates the behaviour of the original
   # build script which wasn't able to load top
   # level elements from any file
-  file="$1"
-
-  if [ -f "$file" ]; then
-    unicat "$file" \
-    | sed -r ':X; $bY; N; bX; :Y;
-              s:<(\?[xX][mM][lL]|!DOCTYPE)[[:space:]]+[^>]+>::g
-              s:<[^!][^>]*>::;
-              s:</[^>]*>([^<]*((<[^>]+/>|<!([^>]|<[^>]*>)*>|<\?[^>]+>)[^<]*)*)?$:\1:;'
+  if [ -f "$1" ]; then
+    sed -r ':X; $bY; N; bX; :Y;
+            s:<(\?[xX][mM][lL]|!DOCTYPE)[[:space:]]+[^>]+>::g
+            s:<[^!][^>]*>::;
+            s:</[^>]*>([^<]*((<[^>]+/>|<!([^>]|<[^>]*>)*>|<\?[^>]+>)[^<]*)*)?$:\1:;' "$1"
   fi
 }
 
 get_attributes(){
   # get attributes of top level element in a given
   # XHTML file
-  file="$1"
-
   sed -rn ':X; N; $!bX;
-           s;^.*<[\n\t\r ]*([xX]|[xX]?[hH][tT])[mM][lL][\n\t\r ]+([^>]*)>.*$;\2;p' \
-    "$file"
+           s;^.*<[\n\t\r ]*([xX]|[xX]?[hH][tT])[mM][lL][\n\t\r ]+([^>]*)>.*$;\2;p' "$1"
 }
 

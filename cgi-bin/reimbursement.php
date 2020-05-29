8b71dcb297a73899480e1d378aefe2a428371c5a
max.mehl <max.mehl@fsfe.org>
2020-05-29 12:08:04 +0200

    sanitise attachment filename

--- cgi-bin/reimbursement.php
+++ cgi-bin/reimbursement.php
@@ -41,6 +41,54 @@ $who = explode('|', $who)[1];
 function errexit($msg) {
   exit("Error: " . $msg . "<br/><br/>To avoid losing your data, press the back button in your browser");
 }
+/* Snippet Begin:
+ * SPDX-SnippetLicenseConcluded: CC-BY-SA-4.0
+ * SPDX-SnippetCopyrightText: mgutt <https://stackoverflow.com/users/318765/mgutt>
+*/
+function filter_filename($filename, $beautify=true) {
+  // sanitize filename
+  $filename = preg_replace(
+    '~
+    [<>:"/\\|?*]|            # file system reserved https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
+    [\x00-\x1F]|             # control characters http://msdn.microsoft.com/en-us/library/windows/desktop/aa365247%28v=vs.85%29.aspx
+    [\x7F\xA0\xAD]|          # non-printing characters DEL, NO-BREAK SPACE, SOFT HYPHEN
+    [#\[\]@!$&\'()+,;=]|     # URI reserved https://tools.ietf.org/html/rfc3986#section-2.2
+    [{}^\~`]                 # URL unsafe characters https://www.ietf.org/rfc/rfc1738.txt
+    ~x',
+    '-', $filename);
+  // avoids ".", ".." or ".hiddenFiles"
+  $filename = ltrim($filename, '.-');
+  // optional beautification
+  if ($beautify) $filename = beautify_filename($filename);
+  // maximize filename length to 255 bytes http://serverfault.com/a/9548/44086
+  $ext = pathinfo($filename, PATHINFO_EXTENSION);
+  $filename = mb_strcut(pathinfo($filename, PATHINFO_FILENAME), 0, 255 - ($ext ? strlen($ext) + 1 : 0), mb_detect_encoding($filename)) . ($ext ? '.' . $ext : '');
+  return $filename;
+}
+function beautify_filename($filename) {
+  // reduce consecutive characters
+  $filename = preg_replace(array(
+    // "file   name.zip" becomes "file-name.zip"
+    '/ +/',
+    // "file___name.zip" becomes "file-name.zip"
+    '/_+/',
+    // "file---name.zip" becomes "file-name.zip"
+    '/-+/'
+  ), '-', $filename);
+  $filename = preg_replace(array(
+    // "file--.--.-.--name.zip" becomes "file.name.zip"
+    '/-*\.-*/',
+    // "file...name..zip" becomes "file.name.zip"
+    '/\.{2,}/'
+  ), '.', $filename);
+  // lowercase for windows/unix interoperability http://support.microsoft.com/kb/100625
+  $filename = mb_strtolower($filename, mb_detect_encoding($filename));
+  // ".file-name.-" becomes "file-name"
+  $filename = trim($filename, '.-');
+  return $filename;
+}
+/* Snippet End */ 
+
 
 // Sanity checks for parameters
 if ($type == "rc") {
@@ -127,7 +175,7 @@ foreach ($entry as $key => $date) {  // run over each row
 
   // Set name and temporary destination for attached receipt
   $receipt_ext = pathinfo($receipt_name)['extension'];
-  $receipt_rename = $type_date ."-". $type ."-". $who ."-receipt-". $receipt_no ."-". $er[$key] .".". "$receipt_ext";
+  $receipt_rename = filter_filename($type_date ."-". $type ."-". $who ."-receipt-". $receipt_no ."-". $er[$key] .".". "$receipt_ext");
   $receipt_dest[$key] = "/tmp/" . $receipt_rename;
 
   // Try to move file to temporary destination
@@ -162,7 +210,7 @@ foreach ($entry as $key => $date) {  // run over each row
 foreach ($csv as $fields) {
   fputcsv($csvfile, $fields, ',', '"', '"');
 }
-$email->addAttachment($csvfile_path, $type_date ."-". $type ."-". $who . ".csv");
+$email->addAttachment($csvfile_path, filter_filename($type_date ."-". $type ."-". $who . ".csv"));
 
 // Prepare email body
 $email_body = "Hi,

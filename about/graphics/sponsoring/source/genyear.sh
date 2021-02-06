60694dea76209ae7fd019013cca692647216b5f6
max.mehl <max.mehl@fsfe.org>
2021-02-06 01:46:58 +0100

    remove obsolete XHTML output, and adopt to latest CLI parameters (Inkscape 1.0.1)

--- about/graphics/sponsoring/source/genyear.sh
+++ about/graphics/sponsoring/source/genyear.sh
@@ -14,41 +14,25 @@ if [ -z "$year" ]; then
 fi
 [ -z "$odir" ] && odir=.
 
-xhtml="${odir}/xhtml"
-
-echo "<h2>$year Buttons</h2>" >"$xhtml"
-
 for type in Donor BronzeDonor SilverDonor GoldDonor; do
-  echo "  <h3>$type $year</h3>\n"\
-       "   <p class=\"indent\">\n"\
-       "     <img src=\"${odir}/${type}${year}_w_medium.png\" border=\"0\" alt=\"$type $year button\"/><br />"\
-	>>"$xhtml"
 
-  echo "      white background:" >>"$xhtml"
   for size in huge=900 large=600 medium=300 small=200; do
     s_nam=$(echo $size |cut -d= -f1)
     s_num=$(echo $size |cut -d= -f2)
     sed -r "s:#DONORTYPE#:${type}:;s:#YEAR#:${year}:" "$template" >"${tempfile}"
 
     fname="${odir}/${type}${year}_w_${s_nam}.png"
-    inkscape -z -C -e "${fname}" -d 300 -w $s_num -b "#FFFFFF" "$tempfile"
-
-    echo "      [<a href=\"${fname}\">${s_nam}; png; $(( $(stat -c %s "${fname}") / 1024 ))kb</a>]" >>"$xhtml"
+    inkscape -C -o "${fname}" -d 300 -w $s_num -b "#FFFFFF" "$tempfile"
   done
 
-  echo "      transparent background:" >>"$xhtml"
   for size in huge=900 large=600 medium=300 small=200; do
     s_nam=$(echo $size |cut -d= -f1)
     s_num=$(echo $size |cut -d= -f2)
     sed -r "s:#DONORTYPE#:${type}:;s:#YEAR#:${year}:" "$template" >"${tempfile}"
 
     fname="${odir}/${type}${year}_t_${s_nam}.png"
-    inkscape -z -C -e "${fname}" -d 300 -w $s_num "$tempfile"
-
-    echo "      [<a href=\"${fname}\">${s_nam}; png; $(( $(stat -c %s "${fname}") / 1024 ))kb</a>]" >>"$xhtml"
+    inkscape -C -o "${fname}" -d 300 -w $s_num "$tempfile"
   done
-
-  echo "    </p>" >>"$xhtml"
 done
 
 rm "${tempfile}"

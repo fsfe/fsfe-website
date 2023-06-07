5228b62e15b7100271b2b5bf50bf6b209b40825a
Tobias Diekershoff <tobiasd@fsfe.org>
2023-06-07 11:09:18 +0200

    no attachment for now

--- cgi-bin/promotion.php
+++ cgi-bin/promotion.php
@@ -263,14 +263,14 @@ $jsondata = [
               "email" => $_POST['mail'],
               "firstName" => $_POST['firstname'],
               "lastName" => $_POST['lastname'],
-          ],
+          ]/*,
           "attachments" => [
               [
                   "fileName" => "letter.odt",
                   "mimeType" => "application/vnd.oasis.opendocument.text",
                   "data"     => base64_encode(file_get_contents($outfile))
               ]
-          ]
+          ]*/
       ]
   ],
   "imported"     => false,

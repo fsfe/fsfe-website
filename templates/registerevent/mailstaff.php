543f769aa365f4158e0edcd107605dfa27076d04
Tobias Diekershoff <tobiasd@fsfe.org>
2023-06-15 14:43:38 +0200

    indentation

--- templates/registerevent/mailstaff.php
+++ templates/registerevent/mailstaff.php
@@ -3,10 +3,10 @@ there is a new event registered on https://fsfe.org/events/tools/eventregistrati
 
 <p>Below is a list of the information that were provided.</p>
 <ul>
-	<li>Name: <?=$name?></li>
-	<li>Email: <?=$email?></li>
+    <li>Name: <?=$name?></li>
+    <li>Email: <?=$email?></li>
     <li>Event Title: <?=$title?></li>
-	<li>Location: <?=$location?></li>
+    <li>Location: <?=$location?></li>
 </ul>
  
 <p>Find attached the event data as XML file for the webpage and data file for the Wiki. Please upload the

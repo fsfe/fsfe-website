7f648db0fd3dcbb49f5361e51ae34261c65160fb
Darragh Elliott <delliott@fsfe.org>
2024-05-17 16:17:06 +0200

    Enable automatic prs for adding events

--- templates/registerevent/event.php
+++ templates/registerevent/event.php
@@ -2,23 +2,33 @@
 
 <eventset>
   <version>1</version>
-  <event start="<?php echo htmlspecialchars($startdate) ?>" end="<?php if($enddate) { echo htmlspecialchars($enddate); } else { echo htmlspecialchars($startdate); }?>">
-    <?php if($online === "yes") { $location = " ($location)"; } else { $location = " in " . $location; } ?>
+  <event start="<?php echo htmlspecialchars($startdate) ?>" end="<?php if($enddate) {
+      echo htmlspecialchars($enddate);
+  } else {
+      echo htmlspecialchars($startdate);
+  }?>">
+    <?php if($online === "yes") {
+        $location = " ($location)";
+    } else {
+        $location = " in " . $location;
+    } ?>
     <title><?php echo htmlspecialchars($title) . $location ?></title>
 
     <body>
-      <p><?php echo str_replace("</p><p>","</p>\n      <p>",nl2br(preg_replace("/(\r?\n){2,}/", "</p><p>", htmlspecialchars($description)))); ?></p>
+      <p><?php echo str_replace("</p><p>", "</p>\n      <p>", nl2br(preg_replace("/(\r?\n){2,}/", "</p><p>", htmlspecialchars($description)))); ?></p>
     </body>
 
-    <?php if($url) { echo "<link>" . htmlspecialchars($url) . "</link>"; }?>
+    <?php if($url) {
+        echo "<link>" . htmlspecialchars($url) . "</link>";
+    }?>
 
     <tags>
       <?php if($online !== "yes") { ?><tag key="<?php echo strtolower(htmlspecialchars($countrycode)); ?>"><?php echo htmlspecialchars($countryname); ?></tag><?php } ?>
       <?php
         foreach ($tags as $tag) {
-          echo sprintf('<tag key="%s"/>', htmlspecialchars($tag)) . "\n";
+            echo sprintf('<tag key="%s"/>', htmlspecialchars($tag)) . "\n";
         }
-      ?>
+?>
       <tag key="front-page"/>
     </tags>
   </event>

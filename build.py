53775f0fe4623f21c9b57523978bc5405c124d16
Darragh Elliott <me@delliott.xyz>
2025-01-20 12:40:07 +0000

    feat: phase2 in python run pre phase1 and phase 1 directly from build.py use uv for dep management

--- build.py
+++ build.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run
+
+# /// script
+# dependencies = [
+# # XML parser
+# "lxml==5.3.0",
+# # For getting english language names of languages from two letter codes.
+# "python-iso639==2024.10.22",
+# # For stopwords for the search index
+# "nltk==3.9.1",
+# # For minification html, css and js
+# "tdewolff-minify==2.20.37",
+# ]
+# ///
+
 
 import argparse
 import logging
@@ -7,106 +21,49 @@ import subprocess
 import sys
 from pathlib import Path
 
-from tools.serve_websites import serve_websites
+
+from build.full import full
+from build.parse_arguments import parse_arguments
+from build.phase1.run import phase1_run
+from build.phase2.run import phase2_run
+from build.serve_websites import serve_websites
+from build.update import update
 
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    """
-    Main process of the website builder
-    """
-    # Change to the dir the script is in.
-    os.chdir(os.path.dirname(__file__))
 
-    parser = argparse.ArgumentParser(
-        description="Python script to handle building of the fsfe webpage"
+def main(args: argparse.Namespace):
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=args.log_level,
     )
-    parser.add_argument(
-        "--target",
-        dest="target",
-        help="Directory to build websites into.",
-        type=str,
-        default="./output/final",
+    logger.debug(args)
+
+    if args.full:
+        full()
+    if args.update:
+        update()
+
+    languages = (
+        args.languages.split(",")
+        if args.languages is not None
+        else list(map(lambda path: path.name, Path("global/languages").glob("??")))
     )
-    parser.add_argument(
-        "--log-level",
-        dest="log_level",
-        type=str,
-        default="INFO",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        help="Set the logging level (default: INFO)",
+
+    working_target = Path(
+        "./output/stage"
+        if args.stage or "@" in args.target or ":" in args.target or "," in args.target
+        else args.target
     )
-    parser.add_argument(
-        "--full",
-        dest="full",
-        help="Force a full rebuild of all webpages.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--update",
-        dest="update",
-        help="Update the repo as part of the build.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--languages",
-        dest="languages",
-        help="Languages to build website in.",
-        type=str,
-    )
-    # parser.add_argument(
-    #     "--status",
-    #     dest="status",
-    #     help="Store status reports.",
-    #     action="store_true",
-    # )
-    parser.add_argument(
-        "--status-dir",
-        dest="status_dir",
-        help="Directory to store status reports in.",
-        type=Path,
-    )
-    parser.add_argument(
-        "--stage",
-        dest="stage",
-        help="Force the use of an internal staging directory.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--test",
-        dest="test",
-        help="Enable some testing features that test for worse scenarios, but hamper performance.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--serve",
-        dest="serve",
-        help="Serve the webpages after rebuild",
-        action="store_true",
-    )
-    args = parser.parse_args()
-    logging.basicConfig(format="*   %(message)s", level=args.log_level)
-    Path("./output").mkdir(parents=True, exist_ok=True)
-    if not (args.full and args.update):
-        command = (
-            "build_run"
-            if not args.full or args.update
-            else "build_into"
-            if args.full
-            else "git_build_into"
-        )
-    else:
-        logger.critical("Cannot do a full rebuild and an update at once, exiting")
-        sys.exit(1)
-    if not args.status_dir:
-        args.status_dir = (
-            f"{args.target.removesuffix('/')}/status.fsfe.org/fsfe.org/data"
-        )
-    logger.debug(f"Args: {args}")
+
+    phase1_run(languages)
+    phase2_run(languages, working_target)
+
     to_run = (
         [
             "./build/build_main.sh",
-            command,
+            "build_run",
             args.target,
         ]
         + (
@@ -135,14 +92,19 @@ if __name__ == "__main__":
         )
     )
     logger.debug(f"Subprocess command: {to_run}")
-    env = dict(os.environ)
-    env["LOGLEVEL"] = args.log_level
-    if args.test:
-        env["TEST"] = str(args.test).upper()
-    logger.debug(f"Env Vars being set: {env}")
-    build = subprocess.run(to_run, env=env)
+    build = subprocess.run(to_run)
     if build.returncode == 1:
         logger.critical("Build process has failed, Exiting!")
         sys.exit(1)
     if args.serve:
         serve_websites(args.target, 2000, 100)
+
+
+if __name__ == "__main__":
+    """
+    Main process of the website builder
+    """
+    # Change to the dir the script is in.
+    os.chdir(os.path.dirname(__file__))
+    args = parse_arguments()
+    main(args)

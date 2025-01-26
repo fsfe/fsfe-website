368da10de1ad5bea574f9baec14fe9bdbe286b6b
Darragh Elliott <me@delliott.xyz>
2025-01-26 16:40:29 +0000

    feat: rmeove unneeded update stub, implement full rebuild logic

--- build/full.py
+++ build/full.py
@@ -1,7 +1,22 @@
 import logging
+import subprocess
+import sys
 
 logger = logging.getLogger(__name__)
 
 
 def full() -> None:
-    logger.info("Full rebuild stub!")
+    """
+    Git clean the repo to remove all cached artifacts
+    Excluded the root .venv repo, as removing it mid build breaks the build, obviously
+    """
+    logger.info("Performing a full rebuild")
+    result = subprocess.run(
+        ["git", "clean", "-fdx", "--exclude", "/.venv"],
+        capture_output=True,
+        universal_newlines=True,
+    )
+    if result.returncode != 0:
+        logger.critical("git clean failed with message:")
+        logger.critical(result.stderr)
+        sys.exit(1)

88883859ca9ccdfd16e732dff259b2ae9f0f575c
Darragh Elliott <me@delliott.xyz>
2025-01-27 09:27:56 +0000

    feat: move process file to general use folder, rename build_rss* to process_rss* remove now unneeded commented out code from xhtml|rss processing Multithread xhtml processing again

--- build/phase2/run.py
+++ build/phase2/run.py
@@ -4,10 +4,10 @@
 import logging
 from pathlib import Path
 
-from .build_rss_ics_files import build_rss_ics_files
 from .copy_files import copy_files
 from .create_index_symlinks import create_index_symlinks
 from .create_language_symlinks import create_language_symlinks
+from .process_rss_ics_files import process_rss_ics_files
 from .process_xhtml_files import process_xhtml_files
 
 logger = logging.getLogger(__name__)
@@ -21,5 +21,5 @@ def phase2_run(languages: list[str], target: Path):
     process_xhtml_files(languages, target)
     create_index_symlinks(target)
     create_language_symlinks(target)
-    build_rss_ics_files(languages, target)
+    process_rss_ics_files(languages, target)
     copy_files(target)

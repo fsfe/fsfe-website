4dc9a47ed945d8816b0a8de4e7f347130754fd64
Darragh Elliott <me@delliott.xyz>
2025-02-12 10:18:47 +0000

    feat: use internal lib function for subprocesses

--- build/phase0/full.py
+++ build/phase0/full.py
@@ -1,6 +1,6 @@
 import logging
-import subprocess
-import sys
+
+from build.lib.misc import run_command
 
 logger = logging.getLogger(__name__)
 
@@ -10,13 +10,7 @@ def full() -> None:
     Git clean the repo to remove all cached artifacts
     Excluded the root .venv repo, as removing it mid build breaks the build, obviously
     """
-    logger.info("Performing a full rebuild")
-    result = subprocess.run(
+    logger.info("Performing a full rebuild, git cleaning")
+    run_command(
         ["git", "clean", "-fdx", "--exclude", "/.venv"],
-        capture_output=True,
-        universal_newlines=True,
     )
-    if result.returncode != 0:
-        logger.critical("git clean failed with message:")
-        logger.critical(result.stderr)
-        sys.exit(1)

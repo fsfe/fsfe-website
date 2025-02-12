c220ed87ec8bb15dfec7f62410197e3337e4adbe
Darragh Elliott <me@delliott.xyz>
2025-02-12 11:18:01 +0000

    chore: update python deps, and silence pip update warnings

--- shell.nix
+++ shell.nix
@@ -44,6 +44,7 @@ pkgs.mkShell {
     })
   ];
   shellHook = ''
+    export PIP_DISABLE_PIP_VERSION_CHECK=1;
     python -m venv .venv
     source .venv/bin/activate
     pip install -r requirements.txt

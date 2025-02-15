ace7744a1592512d685b8db119e7d94779a7b291
Darragh Elliott <me@delliott.xyz>
2025-02-15 20:11:40 +0000

    feat: multithread the rsync to stage

--- build/phase3/stage_to_target.py
+++ build/phase3/stage_to_target.py
@@ -1,25 +1,29 @@
 import logging
+import multiprocessing
+from pathlib import Path
 
 from build.lib.misc import run_command
 
 logger = logging.getLogger(__name__)
 
 
-def stage_to_target(stagedir: str, targets: str) -> None:
+def _rsync(stagedir: Path, target: str) -> None:
+    run_command(
+        [
+            "rsync",
+            "-av",
+            "--copy-unsafe-links",
+            "--del",
+            str(stagedir) + "/",
+            target,
+        ]
+    )
+
+
+def stage_to_target(stagedir: Path, targets: str, pool: multiprocessing.Pool) -> None:
     """
     Git clean the repo to remove all cached artifacts
     Excluded the root .venv repo, as removing it mid build breaks the build, obviously
     """
     logger.info("Rsyncing from stage dir to target dir(s)")
-    for target in targets.split(","):
-        logger.debug(f"Rsyncing to {target}")
-        run_command(
-            [
-                "rsync",
-                "-av",
-                "--copy-unsafe-links",
-                "--del",
-                str(stagedir) + "/",
-                target,
-            ]
-        )
+    pool.starmap(_rsync, map(lambda target: (stagedir, target), targets.split(",")))

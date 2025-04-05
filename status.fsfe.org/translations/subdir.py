8a66e37ca9bfc5dc1abf9e0a8115b89c44871b8c
Darragh Elliott <me@delliott.xyz>
2025-04-05 15:49:03 +0100

    feat: add spdx headers for reuse compliance

--- status.fsfe.org/translations/subdir.py
+++ status.fsfe.org/translations/subdir.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Free Software Foundation Europe e.V. <https://fsfe.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import datetime
 import logging
 import multiprocessing

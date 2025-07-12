4596d20639062946ea58d28791ec3bf99a30a909
Darragh Elliott <me@delliott.xyz>
2025-07-12 20:35:41 +0100

    build: force use of system python from nix shell solves an ssl certs issue with nltk

--- shell.nix
+++ shell.nix
@@ -8,53 +8,47 @@ let
   treefmt-nixSrc = builtins.fetchTarball "https://github.com/numtide/treefmt-nix/archive/refs/heads/master.tar.gz";
   treefmt-nix = import treefmt-nixSrc;
 in
-(pkgs.buildFHSEnv {
-  name = "simple-env";
-  # Installed for host pc only
-  targetPkgs =
-    pkgs:
-    (with pkgs; [
-      # For getting python deps
-      uv
-      # needed by lxml
-      libxslt
-      libxml2
-      # For less compilation
-      lessc
-      # Needed for git clean in full rebuilds
-      git
-      # Needed for compiling minifiers
-      libffi
-      go
-      # Formatter
-      (treefmt-nix.mkWrapper pkgs {
-        # Used to find the project root
-        projectRootFile = "shell.nix";
-        enableDefaultExcludes = true;
-        programs = {
-          ruff-check.enable = true;
-          ruff-format.enable = true;
-          nixfmt.enable = true;
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    # For getting python deps
+    uv
+    python312
+    # needed by lxml
+    libxslt
+    libxml2
+    # For less compilation
+    lessc
+    # Needed for git clean in full rebuilds
+    git
+    # Needed for compiling minifiers
+    libffi
+    go
+    # Formatter
+    (treefmt-nix.mkWrapper pkgs {
+      # Used to find the project root
+      projectRootFile = "shell.nix";
+      enableDefaultExcludes = true;
+      programs = {
+        ruff-check.enable = true;
+        ruff-format.enable = true;
+        nixfmt.enable = true;
+      };
+      settings = {
+        global = {
+          on-unmatched = "debug";
+          excludes = [
+            ".nltk_data"
+            ".venv"
+          ];
         };
-        settings = {
-          global = {
-            on-unmatched = "debug";
-            excludes = [
-              ".nltk_data"
-              ".venv"
-            ];
-          };
-        };
-      })
-      # Packages for git hooks
-      mediainfo
-      perl
-      file
-    ]);
-  # Installed for every architecture: only install the lib outputs
-  multiPkgs =
-    pkgs:
-    (with pkgs; [
-    ]);
-  # runScript = '''';
-}).env
+      };
+    })
+    # Packages for git hooks
+    mediainfo
+    perl
+    file
+  ];
+  shellHook = ''
+    export UV_SYSTEM_PYTHON=true
+  '';
+}

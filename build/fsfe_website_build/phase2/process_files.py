04b20667a96ee68f9b44f2d2b802708e26edd996
Darragh Elliott <me@delliott.net>
2025-10-11 21:47:30 +0000

    refactor: inline unneeded function streamline processor choosing logic

--- build/fsfe_website_build/phase2/process_files.py
+++ build/fsfe_website_build/phase2/process_files.py
@@ -4,6 +4,7 @@
 
 import logging
 import multiprocessing.pool
+from itertools import product
 from pathlib import Path
 
 from lxml import etree
@@ -12,44 +13,6 @@ from fsfe_website_build.lib.misc import get_basepath
 from fsfe_website_build.lib.process_file import process_file
 
 logger = logging.getLogger(__name__)
-
-
-def _run_process(
-    target_file: Path,
-    processor_tuple: tuple[Path, etree.XSLT],
-    source_file: Path,
-    basepath: Path,
-    lang: str,
-) -> None:
-    # if the target file does not exist, we make it
-    if not target_file.exists() or any(
-        # If any source file is newer than the file to be generated
-        # we recreate the generated file
-        # if the source file does not exist, ignore it.
-        (
-            (file.exists() and file.stat().st_mtime > target_file.stat().st_mtime)
-            for file in [
-                (
-                    source_file
-                    if source_file.exists()
-                    else Path(str(basepath) + ".en.xhtml")
-                ),
-                processor_tuple[0],
-                (
-                    source_file.parent.joinpath("." + basepath.name).with_suffix(
-                        ".xmllist",
-                    )
-                ),
-                Path(f"global/data/texts/.texts.{lang}.xml"),
-                Path(f"global/data/topbanner/.topbanner.{lang}.xml"),
-                Path("global/data/texts/texts.en.xml"),
-            ]
-        ),
-    ):
-        logger.debug("Building %s", target_file)
-        result = process_file(source_file, processor_tuple[1])
-        target_file.parent.mkdir(parents=True, exist_ok=True)
-        result.write_output(target_file)
 
 
 def _process_set(
@@ -66,18 +29,43 @@ def _process_set(
     parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
     xslt_tree = etree.parse(processor.resolve(), parser)
     transform = etree.XSLT(xslt_tree)
-    for basepath in files:
-        for lang in languages:
-            source_file = Path(str(basepath) + f".{lang}.xhtml")
-            target_suffix = (
-                ".html" if (len(processor.suffixes) == 1) else processor.suffixes[0]
-            )
-            target_file = target.joinpath(
-                source_file.relative_to(source_dir),
-            ).with_suffix(target_suffix)
-            _run_process(
-                target_file, (processor, transform), source_file, basepath, lang
-            )
+    for basepath, lang in product(files, languages):
+        source_file = Path(str(basepath) + f".{lang}.xhtml")
+        target_suffix = (
+            ".html" if (len(processor.suffixes) == 1) else processor.suffixes[0]
+        )
+        target_file = target.joinpath(
+            source_file.relative_to(source_dir),
+        ).with_suffix(target_suffix)
+        # if the target file does not exist, we make it
+        if not target_file.exists() or any(
+            # If any source file is newer than the file to be generated
+            # we recreate the generated file
+            # if the source file does not exist, ignore it.
+            (
+                (file.exists() and file.stat().st_mtime > target_file.stat().st_mtime)
+                for file in [
+                    (
+                        source_file
+                        if source_file.exists()
+                        else Path(str(basepath) + ".en.xhtml")
+                    ),
+                    processor,
+                    (
+                        source_file.parent.joinpath("." + basepath.name).with_suffix(
+                            ".xmllist",
+                        )
+                    ),
+                    Path(f"global/data/texts/.texts.{lang}.xml"),
+                    Path(f"global/data/topbanner/.topbanner.{lang}.xml"),
+                    Path("global/data/texts/texts.en.xml"),
+                ]
+            ),
+        ):
+            logger.debug("Building %s", target_file)
+            result = process_file(source_file, transform)
+            target_file.parent.mkdir(parents=True, exist_ok=True)
+            result.write_output(target_file)
 
 
 def process_files(
@@ -103,30 +91,36 @@ def process_files(
 
     # This gathers all the simple xhtml files for generating xhtml output
     for file in source_dir.glob("**/*.*.xhtml"):
-        specific_processor = (
+        # Processors with a file ending for the output encoded in the name, eg
+        # events.rss.xsl
+        type_specific_processors = set(
+            file.parent.glob(f"{get_basepath(file).name}.*.xsl")
+        )
+        # The form that a named processor for a file would be
+        # Essentially, if there is no output name, we default to html
+        # <basename>.html.xsl should be equal in effect to <basename>.xsl
+        xhtml_named_processor = (
             file.with_suffix("")
             .with_suffix(".xsl")
             .resolve()
             .relative_to(source_dir.parent.resolve())
         )
-        default_processor = (
+        # if that does not exist, default to
+        xhtml_default_processor = (
             file.parent.joinpath(".default.xsl")
             .resolve()
             .relative_to(source_dir.parent.resolve())
         )
-        used_processor = (
-            specific_processor if specific_processor.exists() else default_processor
+        xhtml_processor = (
+            xhtml_named_processor
+            if xhtml_named_processor.exists()
+            else xhtml_default_processor
         )
-        basepath = get_basepath(file)
-        process_files_dict[used_processor].add(basepath)
-
-    # And now for processors with additional suffixes we (eg events.rss.xsl)
-    # we add the basename
-    # this is for the generation of rss and ics files mainly,
-    # but is extensible by design
-    for processor, files in process_files_dict.items():
-        if len(processor.suffixes) > 1:
-            files.add(get_basepath(processor))
+        # And add the files/processors we should be using for every file
+        for processor in type_specific_processors | {xhtml_processor}:
+            # If we are using a type specific processor, there will be more suffixes
+            file_to_add = processor if len(processor.suffixes) > 1 else file
+            process_files_dict[processor].add(get_basepath(file_to_add))
 
     pool.starmap(
         _process_set,

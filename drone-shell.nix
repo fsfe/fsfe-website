078ffcdc39f30120867ed49877f9e0b62b23b34d
hl <hl@fsfe.org>
2025-11-10 12:15:06 +0000

    Drone: upgrade docker version, sign, add nix shell for signing (#5482)
    Reviewed-on: https://git.fsfe.org/FSFE/fsfe-website/pulls/5482
    Co-authored-by: hl <hl@fsfe.org>
    Co-committed-by: hl <hl@fsfe.org>

---
+++ drone-shell.nix
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2025 Free Software Foundation Europe <https://fsfe.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  # we assume the repo is cloned so git is available
+  # we assume awk is present
+  buildInputs = with pkgs; [
+    drone-cli # signing
+  ];
+  shellHook = ''
+  # GIT
+  if ! git remote get-url upstream > /dev/null 2>&1 ; then
+     # REVIEW(fkobi): this could get annoying if for some reason people do not want the upstream
+     git remote add upstream git@git.fsfe.org:fsfe-system-hackers/hugo-container.git
+
+     git config branch.main.pushRemote origin
+  fi
+
+  # DRONE
+  export DRONE_SERVER=https://drone.fsfe.org
+  export DRONE_TOKEN
+
+  DRONE_FILE=drone_token.txt
+  if [[ -r $DRONE_FILE ]]; then
+     DRONE_TOKEN=$(cat $DRONE_FILE)
+  else
+     echo "Insert your drone token (found at $DRONE_SERVER/account)"
+     read -r -p "Token: " DRONE_TOKEN
+     echo -n $DRONE_TOKEN > $DRONE_FILE
+  fi
+  unset DRONE_FILE
+
+  REPO_NAME="$(git remote get-url origin | awk -F "[:.]" '{printf $4}')"
+  alias save="drone sign --save $REPO_NAME && printf 'Hash saved!' && git add .drone.yml"
+
+  echo "You are working on $REPO_NAME repository."
+  echo "Use the \"save\" alias to drone sign and git commit your drone.yml changes."
+  '';
+}

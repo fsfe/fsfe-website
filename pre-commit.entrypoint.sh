1cf8647cc655f01cdd9a6458a6676f8052a1699e
tobiasd <tobiasd@fsfe.org>
2025-12-04 10:07:06 +0000

    revert 3564d43036c64484524703b5686fe2841ac4e049
    revert pre-commit.entrypoint.sh: skip variable creation
    
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- pre-commit.entrypoint.sh
+++ pre-commit.entrypoint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # Get the changed files
 target_branch="${1:-master}"
 source_branch="$(git rev-parse --abbrev-ref HEAD)"
+files="$(git diff --name-only "$source_branch" "$target_branch")"
 files_args=""
-
-for file in git diff --name-only "$source_branch" "$target_branch"; do
+for file in $files; do
 	if [ -f "$file" ]; then
 		files_args+="--file $file "
 	fi

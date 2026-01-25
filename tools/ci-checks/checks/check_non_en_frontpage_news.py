f012e6dc270ed58a1699792a1ed8e2d944da9311
Darragh Elliott <me@delliott.net>
2026-01-25 11:46:51 +0000

    feat: port frontpage news check to python some minor cleanup of xml_structure check

---
+++ tools/ci-checks/checks/check_non_en_frontpage_news.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: Free Software Foundation Europe e.V. <https://fsfe.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+"""Check that the passed files do not have frontpage news in non english."""
+
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from fsfe_website_build.lib.misc import (
+    get_basepath,
+)
+from lxml import etree
+
+if TYPE_CHECKING:
+    import multiprocessing.pool
+
+logger = logging.getLogger(__name__)
+
+ALLOWED_EXTENSIONS = {".xhtml", ".xml"}
+
+
+def check(files: list[Path], pool: multiprocessing.pool.Pool) -> tuple[bool, str]:  # noqa: ARG001
+    """Check for frontpage news."""
+    failed_files: list[str] = [
+        str(file)
+        for file in files
+        if etree.parse(file).xpath("//tag[@key='front-page']")
+        and not (
+            (file_path := Path(file)).parent
+            / f"{get_basepath(file_path).name}.en{file_path.suffix}"
+        ).exists()
+    ]
+    return (
+        len(failed_files) == 0,
+        "The following files have no english version"
+        " and are marked for the front page:\n" + "\n".join(failed_files),
+    )

e23d967a44f64d550c079c7948b91ca916a826aa
delliott <delliott@fsfe.org>
2026-03-08 10:59:24 +0000

    feat/fedi-redirect (#5682)
    So, this is a basic attempt at implementing the fedi redirects as described on the webpage.
    
    Things I do not understand:
    
    - In what format should we expect the address request to come?
    - how one should determine the "location" param to be set in the header?
    - What actually are the `.well-known` locations we need to redirect to the script?
    
    Note that the php script is completely untested and almost certainly broken: this is a very rough POC
    
    Co-authored-by: Darragh Elliott <me@delliott.net>
    Reviewed-on: https://git.fsfe.org/FSFE/fsfe-website/pulls/5682
    Co-authored-by: delliott <delliott@fsfe.org>
    Co-committed-by: delliott <delliott@fsfe.org>

---
+++ fsfe.org/cgi-bin/webfinger.php
@@ -0,0 +1,33 @@
+<?php
+
+$acc_request = strtolower($_GET['resource'] ?? '');
+$xml_file = $_SERVER['DOCUMENT_ROOT'].'about/people/people.en.xml';
+$people = simplexml_load_file($xml_file);
+if (false === $people) {
+    http_response_code(500);
+    echo 'ERROR: Could not load people file. Please check the path:'.$xml_file;
+
+    exit;
+}
+
+foreach ($people->xpath('//fediverse') as $fedi_elem) {
+    if ('acct:'.strval($fedi_elem) === $acc_request) {
+        // if we have a location, return it and exit
+        if (isset($fedi_elem['handle'])) {
+            // Handles are in the form <username>@<server>
+            $handle = strval($fedi_elem['handle']);
+            $handle_sections = explode('@', $handle);
+            $response = 'Location: https://'.$handle_sections[1].'/.well-known/webfinger?resource=acct:'.$handle;
+            header($response);
+
+            exit;
+        }
+        http_response_code(500);
+        echo 'ERROR: fediverse element has no location attribute.';
+
+        exit;
+    }
+}
+// No matches
+http_response_code(404);
+echo '404 matching person not found.';

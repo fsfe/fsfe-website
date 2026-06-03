26cf41b1f8c9c424121b07ae5806eb01c928ec11
Darragh Elliott <me@delliott.net>
2026-06-03 17:00:52 +0000

    feat: use lxml alpha that supports free threading

--- build/fsfe_website_build/build.py
+++ build/fsfe_website_build/build.py
@@ -6,6 +6,7 @@
 import argparse
 import logging
 import multiprocessing
+import multiprocessing.pool
 import sys
 import tomllib
 from pathlib import Path
@@ -149,7 +150,7 @@ def _run_build(global_build_config: GlobalBuildConfig) -> None:
     )
     logger.debug(global_build_config)
 
-    with multiprocessing.Pool(global_build_config.processes) as pool:
+    with multiprocessing.pool.ThreadPool(global_build_config.processes) as pool:
         logger.info("Starting phase 0 - Global Conditional Setup")
         # These are simple conditional steps that interact directly with args
         if global_build_config.clean_cache:
@@ -159,11 +160,7 @@ def _run_build(global_build_config: GlobalBuildConfig) -> None:
             full(global_build_config.source)
         global_symlinks(
             global_build_config.source,
-            (
-                global_build_config.languages
-                if global_build_config.languages
-                else global_build_config.all_languages
-            ),
+            (global_build_config.languages or global_build_config.all_languages),
             pool,
         )
         # Create our stable config across all sites
@@ -182,12 +179,8 @@ def _run_build(global_build_config: GlobalBuildConfig) -> None:
                 global_build_config,
                 site,
             )
-            languages: list[str] = (
-                global_build_config.languages
-                if global_build_config.languages
-                else sorted(
-                    {lang_from_filename(path) for path in site.glob("**/*.*.xhtml")},
-                )
+            languages: list[str] = global_build_config.languages or sorted(
+                {lang_from_filename(path) for path in site.glob("**/*.*.xhtml")},
             )
             # Now we know our languages, build our site build config
             site_build_config = SiteBuildConfig(languages, site)

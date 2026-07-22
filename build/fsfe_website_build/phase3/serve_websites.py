44cecd255ac8b1f442b3c5eab39a54689a3e7cbf
tobiasd <181+tobiasd@fsfe.org>
2026-07-22 12:45:27 +0000

    revert c4e464ba460e3d827c8440aa26aa070914977644
    revert Fix: url opening (#6013)
    
    open serve urls in a crossplatform way
    open urls after they have started to be served
    
    It did not work for me and @silviarbgl though it worked for @delliott reverting it for now
    
    ---------
    
    Co-authored-by: Darragh Elliott <me@delliott.net>
    Reviewed-on: https://git.fsfe.org/FSFE/fsfe-website/pulls/6013
    Co-authored-by: delliott <420+delliott@fsfe.org>
    Co-committed-by: delliott <420+delliott@fsfe.org>

--- build/fsfe_website_build/phase3/serve_websites.py
+++ build/fsfe_website_build/phase3/serve_websites.py
@@ -7,14 +7,16 @@ import http.server
 import logging
 import multiprocessing
 import os
+import shutil
 import socketserver
-import webbrowser
 from pathlib import Path
+
+from fsfe_website_build.lib.misc import run_command
 
 logger = logging.getLogger(__name__)
 
 
-def _run_webserver(path: Path, port: int) -> None:
+def _run_webserver(path: str, port: int) -> None:
     """Given a path and a port it will serve that dir on that localhost:port."""
     os.chdir(path)
     handler = http.server.SimpleHTTPRequestHandler
@@ -38,13 +40,13 @@ def serve_websites(
         for path in Path(serve_dir).iterdir()
         if (path.is_dir() and (path.name in site_names))
     )
-    serves: list[tuple[Path, int]] = []
+    serves: list[tuple[str, int]] = []
     for index, directory in enumerate(dirs):
         port = base_port + (increment_number * index)
-        serves.append((directory, port))
+        url = f"http://127.0.0.1:{port}"
+        logger.info("%s served at %s", directory.name, url)
+        if shutil.which("xdg-open") is not None:
+            run_command(["xdg-open", url + "/index.en.html"])
+        serves.append((str(directory), port))
     with multiprocessing.Pool(len(serves)) as pool:
-        pool.starmap_async(_run_webserver, serves)
-        for directory, port in serves:
-            url = f"http://127.0.0.1:{port}"
-            logger.info("%s served at %s", directory.name, url)
-            webbrowser.open(url + "/index.en.html")
+        pool.starmap(_run_webserver, serves)
